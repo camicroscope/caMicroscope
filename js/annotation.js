9af4b6f65353f20513a7dc853205b70651dd995d
Shao Li <shaohuan.li@gmail.com>
2012-06-30 15:41:06 +0200

    Added Ellipse and Polygon SVGs

--- js/annotation.js
+++ js/annotation.js
@@ -1,81 +1,58 @@
  // Create annotations if they are contained within our current view
 function showAnnotations(iip,annot)
 {
-    // Sort our annotations by size to make sure it's always possible to interact
-    // with annotations within annotations
+    $("layer").set({styles:{width:iip.wid+'px',height:iip.hei+'px',left:(iip.view.w-iip.wid)/2+'px',top:(iip.view.h-iip.hei)/2+'px'}});
     if( !annot ) return;
     annot.sort( function(a,b){ return (b.w*b.h)-(a.w*a.h); } );
-
+    var svgHtml='<svg xmlns="http://www.w3.org/2000/svg" version="1.1">';
     for( var i=0; i<annot.length; i++ ){
-       console.log(annot[i]);
        switch (annot[i].annotType)
       {
-          case "rect": console.log("rect");
-                 // Check whether iip annotation is within our view
-      if( iip.wid*(annot[i].x+annot[i].w) > iip.view.x &&
-	  iip.wid*annot[i].x < iip.view.x+iip.view.w &&
-	  iip.hei*(annot[i].y+annot[i].h) > iip.view.y &&
-	  iip.hei*annot[i].y < iip.view.y+iip.view.h
-      ){
-	var annotation = new Element('svg', {
-         'id':'svg1',
-          'xmlns': 'http://www.w3.org/2000/svg',
-          'version':'1.1'
-        }).inject( iip.canvas );
-        var svg1=document.getElementById('svg1');
-         var rect = new Element('rect', {
-         'x':iip.wid*annot[i].x,
-          'y':iip.hei*annot[i].y,
-          'width': iip.wid*annot[i].w,
-           'height':iip.hei*annot[i].h,
-           'fill':annot[i].color,
-           'stroke':'black'
-        }).inject(svg1 );
-      }
-	break;
-          case "circle":console.log("circle");
-   console.log(annot[i]);
-       switch (annot[i].annotType)
-      {
-          case "rect": console.log("rect");
-                 // Check whether iip annotation is within our view
-      if( iip.wid*(annot[i].x+annot[i].r) > iip.view.x &&
-	  iip.wid*annot[i].x < iip.view.x+iip.view.w &&
-	  iip.hei*(annot[i].y+annot[i].r) > iip.view.y &&
-	  iip.hei*annot[i].y < iip.view.y+iip.view.h
-      ){
-	var annotation = new Element('svg', {
-         'id':'svg2',
-          'xmlns': 'http://www.w3.org/2000/svg',
-          'version':'1.1'
-        }).inject( iip.canvas );
-        var svg1=document.getElementById('svg2');
-         var circle = new Element('circle', {
-         'x':iip.wid*annot[i].x+iip.wid*annot[i].r,
-          'y':iip.hei*annot[i].y+iip.wid*annot[i].r,
-          'r': iip.wid*annot[i].r,
-          'fill':annot[i].color,
-          'stroke':'black'
-        }).inject(svg2 );
-      }
-	break;
+          case "rect":
+          // Check whether iip annotation is within our view
+	  if( iip.wid*(annot[i].x+annot[i].w) > iip.view.x &&
+	      iip.wid*annot[i].x < iip.view.x+iip.view.w &&
+	      iip.hei*(annot[i].y+annot[i].h) > iip.view.y &&
+	      iip.hei*annot[i].y < iip.view.y+iip.view.h
+	  ){
+           svgHtml+='<rect x="'+iip.wid*annot[i].x+'" y="'+iip.hei*annot[i].y+'" width="'+iip.wid*annot[i].w+'" height="'+iip.wid*annot[i].h+'" stroke="black" stroke-width="2" fill="'+annot[i].color+'"/>';
+	  }
+	  break;
+          case "circle":
+	  if( iip.wid*(annot[i].x+annot[i].r) > iip.view.x &&
+	      iip.wid*annot[i].x < iip.view.x+iip.view.w &&
+	      iip.hei*(annot[i].y+annot[i].r) > iip.view.y &&
+	      iip.hei*annot[i].y < iip.view.y+iip.view.h
+	   ){
+	    svgHtml+='<circle cx="'+iip.wid*annot[i].x+'" cy="'+iip.hei*annot[i].y+'" r="'+iip.wid*annot[i].r+'" stroke="black" stroke-width="2" fill="'+annot[i].color+'"/>';
+	    }
+	  break;
+          case "polygon":
+          var points=annot[i].points;
+          p=String.split(points,' ');
+          svgHtml+='<polygon points="';
+          for (var j=0;j<p.length;j++)
+          {
+             point=String.split(p[j],',');
+             px=point[0]*iip.wid;
+             py=point[1]*iip.hei;
+             svgHtml+=px+','+py+' ';
+	  }
+	  svgHtml+='" style="fill:lime;stroke:purple;stroke-width:1"/>';
+          break;
+          case "ellipse":
+          svgHtml+='<ellipse cx="'+iip.wid*annot[i].cx+'" cy="'+iip.hei*annot[i].cy+'" rx="'+iip.wid*annot[i].rx+'" ry="'+iip.hei*annot[i].ry+'" style="fill:yellow;stroke:purple;stroke-width:2"/>';
+          break;
           case "svg":console.log("svg");
-	if ( iip.wid*annot[i].x> iip.view.x &&
+	 if ( iip.wid*annot[i].x> iip.view.x &&
 	  iip.wid*annot[i].x < iip.view.x+iip.view.w &&
 	  iip.hei*annot[i].y > iip.view.y &&
 	  iip.hei*annot[i].y < iip.view.y+iip.view.h)
 	{
-           var annotation = new Element('img', {
-          'class': 'annotation',
-          'styles': {
-            left: Math.round(iip.wid * annot[i].x),
-            top: Math.round(iip.hei * annot[i].y ),
-            width: iip.view.res/annot[i].res*annot[i].width
-	  },
-          'src': annot[i].svgsrc
-        }).inject( iip.canvas );
 	}
 	break;
       }
     }
+      svgHtml+='</svg>';
+      $("layer").set({html:svgHtml});
 };
