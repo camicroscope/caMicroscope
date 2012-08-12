191dcf72e33e4fda4f00dcfd48f073095fb51aad
Shao Li <shaohuan.li@gmail.com>
2012-08-12 13:01:43 +0800

    Changed The IP address of the Server

--- js/saveAnnotations.js
+++ js/saveAnnotations.js
@@ -1,6 +1,6 @@
 var saveAnnotations=function(iid,annot)
 {
-      var IP="http://localhost";
+      var IP="http://170.140.138.125";
       var jsonRequest = new Request.JSON({url: IP+'/bio/api/annotation.php', onSuccess: function(e){
 		console.log("saved");
 		}}).post({'iid':iid,'annot':annot});
