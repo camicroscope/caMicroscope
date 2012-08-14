a9dac9ad2cf8930fb980f90cffb7c320039b9423
Shao Li <shaohuan.li@gmail.com>
2012-08-14 11:55:35 +0800

    Added The Magnifying Tool

--- js/tool.js
+++ js/tool.js
@@ -2,7 +2,7 @@ var newtool=function(ele,iip)
 {
 	var element = $(ele);//The Element to Trigger Tool Slider
 	if($("slider")) $("slider").destroy();
-	var slider=new Element('div',{id:'slider','class':'slider',styles:"width:150px",html:''}).inject(element);
+	var slider=new Element('div',{id:'slider','class':'slider',styles:"width:180px",html:''}).inject(element);
 	var rectbutton=new Element('img',{'title':'rectangle','src':'images/rect.svg',
 				   'events':{
 		                        'click':function(){drawMarkups('rect',iip.canvas);},
@@ -39,6 +39,13 @@ var newtool=function(ele,iip)
 					'mouseleave':function(){this.removeClass('selected');}
 					   }
 					}).inject(slider);
+	var magnifybutton=new Element('img',{'title':'magnify','src':'images/magnify.svg',
+				   'events':{
+					'click':function(){magnify();},
+					'mouseenter':function(){this.addClass('selected');},
+					'mouseleave':function(){this.removeClass('selected');}
+					   }
+					}).inject(slider);
 	var hidebutton=new Element('img',{'title':'hide','src':'images/hide.svg',
 				   'events':{
 					'mouseenter':function(){this.addClass('selected');iip.svg.toggle();},
@@ -47,3 +54,12 @@ var newtool=function(ele,iip)
 					   }
 					}).inject(slider);
 };
+
+var magnify=function()
+{
+   var magnify=new Element('div',{id:"magnify",styles:{position:"absolute",width:"200px", "border-color":'black',"-webkit-border-radius": "10em", "-moz-border-radius": "10em","border-bottom-color": "transparent",height:"200px","z-index":2}}).inject(document.body);
+   iip.svg.hide();
+   iip.canvas.getElements("img.layer0").addEvent('mouseenter',function(e){ $("magnify").set({styles:{left:e.event.screenX-50,top:e.event.screenY-100},html:'<img width="200" src="'+this.src+'"/>'});});
+   iip.canvas.addEvent('mousedown',function(){iip.svg.show();if($("magnify")) $("magnify").destroy();});
+}
+
