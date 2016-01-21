49a40e62aa0b38f04e952a229fffb0806fdad41b
Ganesh Iyer <lastlegion@gmail.com>
2016-01-21 11:16:29 -0500

    removed mootools

--- js/annotationtools/ToolBar.js
+++ js/annotationtools/ToolBar.js
@@ -8,25 +8,17 @@ var ToolBar = function(element, options){
     this.width = options.width || '270px';
     this.zindex = options.zindex || '100'; //To Make Sure The Tool Appears in the Front
 
-
-    this.annotationActive = true;
+    this.iid = options.iid || null; 
+    this.annotationActive = isAnnotationActive();
 }
 
 
 ToolBar.prototype.createButtons = function(){
-    this.tool = $(this.source);
+    //this.tool = jQ(this.source);
     var tool = jQuery("#"+"tool"); //Temporary dom element while we clean up mootools
     console.log("creating buttons...");
-    /*
-    this.tool.setStyles({
-        'position': 'absolute',
-        'left': this.left,
-        'top': this.top,
-        'width': this.width,
-        'height': this.height,
-        'z-index': this.zindex
-    });
-    */
+
+
     tool.css({
         "position": "absolute", 
         'left': this.left,
@@ -47,34 +39,13 @@ ToolBar.prototype.createButtons = function(){
          * Ganesh
          * Mootools to Jquery for creation of toolbar buttons
          */
-        /*
-        this.rectbutton = new Element('img', {
-            'title': 'Draw Rectangle',
-            'class': 'toolButton firstToolButtonSpace',
-            'src': 'images/rect.svg'
-        }).inject(this.tool); //Create Rectangle Tool
-        */
-        //console.log(this.rectbutton);
-        //console.log(this.source);
-        //this.rectbutton = new Element(rectbutton);    
-
         this.rectbutton = jQuery("<img>", {
             title: "Draw Rectangle", 
             class: "toolButton firstToolButtonSpace", 
             src: "images/rect.svg"
         });
         tool.append(this.rectButton);
-        //tool.append(this.rectTB;
-        //console.log(tool);    
 
-
-        /*
-        this.ellipsebutton = new Element('img', {
-            'title': 'Draw Ellipse',
-            'class': 'toolButton',
-            'src': 'images/ellipse.svg'
-        }).inject(this.tool); //Ellipse Tool
-        */
         this.ellipsebutton = jQuery("<img>", {
             'title': 'Draw Ellipse',
             'class': 'toolButton',
@@ -82,14 +53,6 @@ ToolBar.prototype.createButtons = function(){
         });
         tool.append(this.ellipsebutton);
 
-       
-        /*
-        this.pencilbutton = new Element('img', {
-            'title': 'Draw Freeline',
-            'class': 'toolButton',
-            'src': 'images/pencil.svg'
-        }).inject(this.tool); //Pencil Tool
-        */
         this.pencilbutton = jQuery('<img>', {
             'title': 'Draw Freeline',
             'class': 'toolButton',
@@ -97,27 +60,12 @@ ToolBar.prototype.createButtons = function(){
         });
         tool.append(this.pencilbutton); //Pencil Tool
         
-        
-        /*
-        this.spacer1 = new Element('img', {
-            'class': 'spacerButton',
-            'src': 'images/spacer.svg'
-        }).inject(this.tool);
-        */
         this.spacer1 = jQuery("<img>", {
             'class': 'spacerButton', 
             'src': 'images/spacer.svg'
         });
         tool.append(this.spacer1);
         
-        
-        /*
-        this.measurebutton = new Element('img', {
-            'title': 'Measurement Tool',
-            'class': 'toolButton',
-            'src': 'images/measure.svg'
-        }).inject(this.tool); //Measurement Tool
-        */
         this.measurebutton = jQuery('<img>', {
             'title': 'Measurement Tool',
             'class': 'toolButton',
@@ -125,26 +73,11 @@ ToolBar.prototype.createButtons = function(){
         });
         tool.append(this.measurebutton);
 
-
-        /*
-        this.spacer2 = new Element('img', {
-            'class': 'spacerButton',
-            'src': 'images/spacer.svg'
-        }).inject(this.tool);
-        */
         this.spacer2 = jQuery('<img>', {
             'class': 'spacerButton',
             'src': 'images/spacer.svg'
         });
         tool.append(this.spacer2);
-        
-        /*
-        this.filterbutton = new Element('img', {
-            'title': 'Filter Markups',
-            'class': 'toolButton',
-            'src': 'images/filter.svg'
-        }).inject(this.tool); //Filter Button
-        */
         
         this.filterbutton = jQuery('<img>', {
             'title': 'Filter Markups',
@@ -152,27 +85,14 @@ ToolBar.prototype.createButtons = function(){
             'src': 'images/filter.svg'
         });
         tool.append(this.filterbutton); //Filter Button
-       
-        /*
-        this.hidebutton = new Element('img', {
-            'title': 'Show/Hide Markups',
-            'class': 'toolButton',
-            'src': 'images/hide.svg'
-        }).inject(this.tool); //Show/Hide Button
-        */
+
         this.hidebutton = jQuery('<img>', {
             'title': 'Show/Hide Markups',
             'class': 'toolButton',
             'src': 'images/hide.svg'
         });
         tool.append(this.hidebutton);
-        /*
-        this.fullDownloadButton = new Element('img', {
-            'title': 'Download All Markups (Coming Soon)',
-            'class': 'toolButton',
-            'src': 'images/fullDownload.svg'
-        }).inject(this.tool); //Full Download
-        */
+
         this.fullDownloadButton = jQuery('<img>', {
             'title': 'Download All Markups (Coming Soon)',
             'class': 'toolButton',
@@ -180,14 +100,6 @@ ToolBar.prototype.createButtons = function(){
         });
         tool.append(this.fullDownloadButton);
         
-        /*
-        this.partialDownloadButton = new Element('img', {
-            'title': 'Download Partial Markups (Coming Soon)',
-            'class': 'toolButton',
-            'src': 'images/partDownload.svg'
-        }).inject(this.tool); //Partial Download
-        */
-
         this.partialDownloadButton = jQuery('<img>', {
             'title': 'Download Partial Markups (Coming Soon)',
             'class': 'toolButton',
@@ -195,162 +107,39 @@ ToolBar.prototype.createButtons = function(){
         });
         tool.append(this.partialDownloadButton);  //Partial Download
 
-
-        /*this.savebutton = new Element('img', {
-            'title': 'Save Current State',
-            'class': 'toolButton',
-            'src': 'images/save.svg'
-        }).inject(this.tool); //Save Button
-        */
-    }
-    
-    /*
-    this.titleButton = new Element('<p>',{
-        'class' : 'titleButton',
-        'text' : 'caMicroscope'
-    }).inject(this.tool);
-    */
-    this.titleButton = jQuery('<p>',{
-        'class' : 'titleButton',
-        'text' : 'caMicroscope'
-    });
-    tool.append(this.titleButton);
-
-    /*
-    this.iidbutton = new Element('<p>',{
-        'class':'iidButton',
-        'text':'SubjectID :' + this.iid
-    }).inject(this.tool);
-    */
-    this.iidbutton = jQuery('<p>', {
-        'class': 'iidButton',
-        'text': 'SubjectID :' + this.iid
-    });
-    tool.append(this.iidbutton);
-
-    /* ASHISH - disable quit button
-        this.quitbutton = new Element('img', {
-            'title': 'quit',
-            'class': 'toolButton',
-            'src': 'images/quit.svg'
-        }).inject(this.tool); //Quit Button
-    */
-    if(this.annotationActive)
-    {
+        /*
+         * Event handlers on click for the buttons
+         */
         this.rectbutton.on("click", function(){
             this.mode = 'rect';
             this.drawMarkups();
         }.bind(this));
-        /*
-        this.rectbutton.addEvents({
-            'click': function () {
-                this.mode = 'rect';
-                this.drawMarkups();
-            }.bind(this)
-        }); //Change Mode
-        */
-        
+
         this.ellipsebutton.on("click", function(){
             this.mode = 'ellipse';
             this.drawMarkups();
 
         }.bind(this));
 
-        /*
-        this.ellipsebutton.addEvents({
-            'click': function () {
-                this.mode = 'ellipse';
-                this.drawMarkups();
-            }.bind(this)
-        });
-        */
-      /*  this.polybutton.addEvents({
-            'click': function () {
-                this.mode = 'polyline';
-                this.drawMarkups();
-            }.bind(this)
-        }); */
-        /*
-        this.pencilbutton.addEvents({
-            'click': function () {
-                this.mode = 'pencil';
-                this.drawMarkups();
-            }.bind(this)
-        });
-        */
         this.pencilbutton.on('click', function () {
                 this.mode = 'pencil';
                 this.drawMarkups();
         }.bind(this));
 
-
-        /*
-        this.measurebutton.addEvents({
-            'click': function () {
-                this.mode = 'measure';
-                this.drawMarkups();
-            }.bind(this)
-        });
-        */
         this.measurebutton.on('click', function () {
                 this.mode = 'measure';
                 this.drawMarkups();
         }.bind(this));       
-        /*this.magnifybutton.addEvents({
-            'click': function () {
-                this.mode = 'magnify';
-                this.magnify();
-            }.bind(this)
-        });
-        this.colorbutton.addEvents({
-            'click': function () {
-                this.selectColor()
-            }.bind(this)
-        });*/
-        /*
-        this.hidebutton.addEvents({
-            'click': function () {
-                this.toggleMarkups()
-            }.bind(this)
-        });
-        */
+
         this.hidebutton.on('click', function () {
             this.toggleMarkups()
         }.bind(this));
-        /*
-        this.filterbutton.addEvents({
-            'click': function () {
-                this.removeMouseEvents();
-                this.promptForAnnotation(null, "filter", this, null);
-            }.bind(this)
-        });
-        */
 
         this.filterbutton.on('click', function () {
             this.removeMouseEvents();
             this.promptForAnnotation(null, "filter", this, null);
         }.bind(this));
-        /*this.analyzebutton.addEvents({
-            'click': function () {
-                this.analyze()
-            }.bind(this)
-        });
-        /*this.savebutton.addEvents({
-            'click': function () {
-                this.saveState()
-            }.bind(this)
-        });*/
-        /* ASHISH Disable quit button
-        this.quitbutton.addEvents({
-            'click': function () {
-                this.quitMode();
-                this.quitbutton.hide();
-            }.bind(this)
-        });
-        this.quitbutton.hide(); //Quit Button Will Be Used To Return To the Default Mode
-        */
-       
-        //var toolButtons = document.getElements(".toolButton");
+
         var toolButtons = jQuery(".toolButton");
         toolButtons.each(function(){
             jQuery(this).on({
@@ -384,6 +173,37 @@ ToolBar.prototype.createButtons = function(){
         });
         jQuery("body").append(this.messageBox);
         */
+
+
+
+
+
+    }
+    
+    
+    this.titleButton = jQuery('<p>',{
+        'class' : 'titleButton',
+        'text' : 'caMicroscope'
+    });
+    tool.append(this.titleButton);
+
+
+    this.iidbutton = jQuery('<p>', {
+        'class': 'iidButton',
+        'text': 'SubjectID :' + this.iid
+    });
+    tool.append(this.iidbutton);
+
+    /* ASHISH - disable quit button
+        this.quitbutton = new Element('img', {
+            'title': 'quit',
+            'class': 'toolButton',
+            'src': 'images/quit.svg'
+        }).inject(this.tool); //Quit Button
+    */
+    if(this.annotationActive)
+    {
+
     }
 };
 
