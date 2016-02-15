eceee8713950663bccff9b808b4059be39922699
Ganesh Iyer <lastlegion@gmail.com>
2016-02-15 17:09:42 -0500

    added zoom levels ot restrict zooming to particular levels

--- osdCamicroscope.php
+++ osdCamicroscope.php
@@ -14,15 +14,17 @@ $config = require 'api/Configuration/config.php';
     <!--<link rel="stylesheet" type="text/css" media="all" href="css/jquery-ui.min.css" />-->
     <link rel="stylesheet" type="text/css" media="all" href="css/simplemodal.css" />
 
-    <script src="js/openseadragon/openseadragon-bin-1.0.0/openseadragon.js"></script>
+    <script src="js/openseadragon/openseadragon-bin-2.0.0/openseadragon.js"></script>
     <script src="js/openseadragon/openseadragon-imaginghelper.min.js"></script>
     <script src="js/openseadragon/openseadragon-scalebar.js"></script>
+    <script src="js/openseadragon/openseadragonzoomlevels.js"></script>
     <script type="text/javascript" src="js/mootools/mootools-core-1.4.5-full-nocompat-yc.js"></script>
     <script type="text/javascript" src="js/mootools/mootools-more-1.4.0.1-compressed.js"></script>
     <script src="js/annotationtools/annotools-openseajax-handler.js"></script>
     <script src="js/imagemetadatatools/osdImageMetadata.js"></script>
     <script src="js/annotationtools/ToolBar.js"></script>
     <script src="js/annotationtools/osdAnnotationTools.js"></script>
+    <script src="js/annotationtools/geoJSONHandler.js"></script>
     <script src="js/dependencies/MD5.js"></script>
     <script src="js/dependencies/jquery.js"></script>
     <!--<script src="js/dependencies/jquery-ui.min.js"></script>-->
@@ -70,17 +72,31 @@ $config = require 'api/Configuration/config.php';
       $.noConflict();
       var annotool = null;
       var tissueId = <?php echo json_encode($_GET['tissueId']); ?>;
+
       var imagedata = new OSDImageMetaData({imageId:tissueId});
+      console.log(tissueId);
+      console.log(imagedata);
+      console.log(tissueId);
+      
       var MPP = imagedata.metaData[0];
+        MPP = {
+            "mpp-x":3,
+            "mpp-y": 3
+        };
       var fileLocation = imagedata.metaData[1];
+
+      fileLocation = "/sharmalab/images/TCGA-BRCA-DX/TCGA-02-0001-01Z-00-DX1.83fce43e-42ac-4dcd-b156-2908e75f2e47.svs.dzi";
       var viewer = new OpenSeadragon.Viewer({ 
             id: "viewer", 
             prefixUrl: "images/",
             showNavigator:  false,
 	        zoomPerClick: 2,
-            maxZoomPixelRatio: 4
-	  });
+            maxZoomPixelRatio: 16
+      });
 
+      var zoomLevels = viewer.zoomLevels({
+        levels:[0.001, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+      });
       viewer.addHandler("open", addOverlays);
       viewer.clearControls();
       viewer.open("<?php print_r($config['fastcgi_server']); ?>?DeepZoom=" + fileLocation);
