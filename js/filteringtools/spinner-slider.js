80a8be77ddee8c5c43c53568fd96ccf95f196e60
Ganesh Iyer <lastlegion@gmail.com>
2016-06-16 14:36:36 -0400

    Code Cleanup: converted filterTools code to follow StandardJS style guide

--- js/filteringtools/spinner-slider.js
+++ js/filteringtools/spinner-slider.js
@@ -10,73 +10,68 @@
  * software is used.
  */
 
-var SpinnerSlider =  function() {
+var SpinnerSlider = function () {
+  var idIncrement = 0
 
-    var idIncrement = 0;
+  return function (options) {
+    this.hash = idIncrement++
 
-    return function(options) {
+    var spinnerId = 'wdzt-spinner-slider-spinner-' + this.hash
+    var sliderId = 'wdzt-spinner-slider-slider-' + this.hash
 
-        this.hash = idIncrement++;
+    var value = options.init
 
-        var spinnerId = 'wdzt-spinner-slider-spinner-' + this.hash;
-        var sliderId = 'wdzt-spinner-slider-slider-' + this.hash;
+    options.$element.html(
+      '<div class="wdzt-table-layout wdzt-full-width">' +
+      '    <div class="wdzt-row-layout">' +
+      '        <div class="wdzt-cell-layout">' +
+      '            <input id="' + spinnerId + '" type="text" size="1"' +
+      '                   class="ui-widget-content ui-corner-all"/>' +
+      '        </div>' +
+      '        <div class="wdzt-cell-layout wdzt-full-width">' +
+      '            <div id="' + sliderId + '" class="wdzt-menu-slider">' +
+      '            </div>' +
+      '        </div>' +
+      '    </div>' +
+      '</div>')
 
-        var value = options.init;
+    var $slider = options.$element.find('#' + sliderId)
+      .slider({
+        min: options.min,
+        max: options.sliderMax !== undefined ?
+          options.sliderMax : options.max,
+        step: options.step,
+        value: value,
+        slide: function (event, ui) {
+          /*jshint unused:true */
+          value = ui.value
+          $spinner.spinner('value', value)
+          options.updateCallback(value)
+        }
+      })
+    var $spinner = options.$element.find('#' + spinnerId)
+      .spinner({
+        min: options.min,
+        max: options.max,
+        step: options.step,
+        spin: function (event, ui) {
+          /*jshint unused:true */
+          value = ui.value
+          $slider.slider('value', value)
+          options.updateCallback(value)
+        }
+      })
+    $spinner.val(value)
+    $spinner.keyup(function (e) {
+      if (e.which === 13) {
+        value = $spinner.spinner('value')
+        $slider.slider('value', value)
+        options.updateCallback(value)
+      }
+    })
 
-
-        options.$element.html(
-            '<div class="wdzt-table-layout wdzt-full-width">' +
-            '    <div class="wdzt-row-layout">' +
-            '        <div class="wdzt-cell-layout">' +
-            '            <input id="' + spinnerId + '" type="text" size="1"' +
-            '                   class="ui-widget-content ui-corner-all"/>' +
-            '        </div>' +
-            '        <div class="wdzt-cell-layout wdzt-full-width">' +
-            '            <div id="' + sliderId + '" class="wdzt-menu-slider">' +
-            '            </div>' +
-            '        </div>' +
-            '    </div>' +
-            '</div>');
-
-        var $slider = options.$element.find('#' + sliderId)
-            .slider({
-                min: options.min,
-                max: options.sliderMax !== undefined ?
-                    options.sliderMax : options.max,
-                step: options.step,
-                value: value,
-                slide: function(event, ui) {
-                    /*jshint unused:true */
-                    value = ui.value;
-                    $spinner.spinner('value', value);
-                    options.updateCallback(value);
-                }
-            });
-        var $spinner = options.$element.find('#' + spinnerId)
-            .spinner({
-                min: options.min,
-                max: options.max,
-                step: options.step,
-                spin: function(event, ui) {
-                    /*jshint unused:true */
-                    value = ui.value;
-                    $slider.slider('value', value);
-                    options.updateCallback(value);
-                }
-            });
-        $spinner.val(value);
-        $spinner.keyup(function(e) {
-            if (e.which === 13) {
-                value = $spinner.spinner('value');
-                $slider.slider('value', value);
-                options.updateCallback(value);
-            }
-        });
-
-
-        this.getValue = function() {
-            return value;
-        };
-
-    };
-};
+    this.getValue = function () {
+      return value
+    }
+  }
+}
--- js/filteringtools/spinner.js
+++ js/filteringtools/spinner.js
@@ -10,51 +10,48 @@
  * software is used.
  */
 
-var Spinner =  function() {
-    /**
-     * This class is an improvement over the basic jQuery spinner to support
-     * 'Enter' to update the value (with validity checks).
-     * @param {Object} options Options object
-     * @return {Spinner} A spinner object
-     */
-    return function(options) {
+var Spinner = function () {
+  /**
+   * This class is an improvement over the basic jQuery spinner to support
+   * 'Enter' to update the value (with validity checks).
+   * @param {Object} options Options object
+   * @return {Spinner} A spinner object
+   */
+  return function (options) {
+    options.$element.html('<input type="text" size="1" ' +
+      'class="ui-widget-content ui-corner-all"/>')
 
-        options.$element.html('<input type="text" size="1" ' +
-            'class="ui-widget-content ui-corner-all"/>');
+    var $spinner = options.$element.find('input')
+    var value = options.init
+    $spinner.spinner({
+      min: options.min,
+      max: options.max,
+      step: options.step,
+      spin: function (event, ui) {
+        /*jshint unused:true */
+        value = ui.value
+        options.updateCallback(value)
+      }
+    })
+    $spinner.val(value)
+    $spinner.keyup(function (e) {
+      if (e.which === 13) {
+        if (!this.value.match(/^-?\d?\.?\d*$/)) {
+          this.value = options.init
+        } else if (options.min !== undefined &&
+          this.value < options.min) {
+          this.value = options.min
+        } else if (options.max !== undefined &&
+          this.value > options.max) {
+          this.value = options.max
+        }
+        value = this.value
+        options.updateCallback(value)
+      }
+    })
 
-        var $spinner = options.$element.find('input');
-        var value = options.init;
-        $spinner.spinner({
-            min: options.min,
-            max: options.max,
-            step: options.step,
-            spin: function(event, ui) {
-                /*jshint unused:true */
-                value = ui.value;
-                options.updateCallback(value);
-            }
-        });
-        $spinner.val(value);
-        $spinner.keyup(function(e) {
-            if (e.which === 13) {
-                if (!this.value.match(/^-?\d?\.?\d*$/)) {
-                    this.value = options.init;
-                } else if (options.min !== undefined &&
-                    this.value < options.min) {
-                    this.value = options.min;
-                } else if (options.max !== undefined &&
-                    this.value > options.max) {
-                    this.value = options.max;
-                }
-                value = this.value;
-                options.updateCallback(value);
-            }
-        });
-
-        this.getValue = function() {
-            return value;
-        };
-
-    };
-
-};
+    this.getValue = function () {
+      return value
+    }
+  }
+}
