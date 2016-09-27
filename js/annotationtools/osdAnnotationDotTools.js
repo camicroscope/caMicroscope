d8f88464572e16badb59082eb922ecf82fdbafef
ajasniew <alina.jasniewski@stonybrook.edu>
2016-09-27 16:09:04 -0400

    Add info panel

--- js/annotationtools/osdAnnotationDotTools.js
+++ js/annotationtools/osdAnnotationDotTools.js
@@ -3,49 +3,51 @@ annotools.prototype.drawDots = function() {
 	
     // alert('comming soon...');
     var self =this;
- 
+	
     var markup_svg = document.getElementById('markups');
     if (markup_svg) {
-      // console.log("destroying")
-      markup_svg.destroy();
+        // console.log("destroying")
+        markup_svg.destroy();
     }
 
-    var container = document.getElementsByClassName(this.canvas)[0]; // Get The Canvas Container
+    var container = document.getElementsByClassName(this.canvas)[0]; // get the Canvas Container
     // console.log(container);
     var width = parseInt(container.offsetWidth);
     var height = parseInt(container.offsetHeight);
 
     /* svgHtml */
-    var svgHtml = '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + 'px" height="' + height + 'px" version="1.1" id="markups" style="border: 10px solid #ffff00">';
-    svgHtml += '<g id="groupcenter"/>';
-    svgHtml += '<g id="origin">';
+    var svgHtml = '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + 'px" height="' + height + 'px" version="1.1" id="markups" style="border: 2px solid #ffff00">';
+        svgHtml += '<g id="groupcenter"/>';
+        svgHtml += '<g id="origin">';
     var origin = viewer.viewport.pixelFromPoint(new OpenSeadragon.Point(.5, .5));
-    svgHtml += '<ellipse id="originpt" cx="' + origin.x + '" cy="' + origin.y + '" rx="' + 4 + '" ry="' + 4 + '" style="display: none"/>';
-    svgHtml += '</g>';
-    svgHtml += '<g id="viewport" transform="translate(0,0)">';
-    svgHtml += '</g>';
+        svgHtml += '<ellipse id="originpt" cx="' + origin.x + '" cy="' + origin.y + '" rx="' + 4 + '" ry="' + 4 + '" style="display: none"/>';
+        svgHtml += '</g>';
+        svgHtml += '<g id="viewport" transform="translate(0,0)">';
+        svgHtml += '</g>';
 
     this.svg = new Element('div', {
-      styles: {
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '100%',
-        height: '100%'
-      },
-      html: svgHtml
+        styles: {
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%'
+        },
+        html: svgHtml
     }).inject(container);
 	
-	// prevent zoom when the SVG overlay is click
+    // prevent zoom when the SVG overlay is clicked
     jQuery('#markups').mousedown(function (event) {
         //console.log(event.which);
         event.preventDefault();
         event.stopPropagation();
         event.stopImmediatePropagation();
     });
-
     
-    // d3 start
+    // hide the Magnifying Tool
+    self.magnifyGlass.hide();
+    
+    // d3.js
     var svgHtmlDot = d3.select('svg');
     var viewPort =  d3.select('#viewport');
    
@@ -61,29 +63,34 @@ annotools.prototype.drawDots = function() {
         console.log(xPosition + ' ' + yPosition);
         
         var svgCircle = circleGroup.append('circle')
-        .attr('cx', xPosition)
-        .attr('cy', yPosition)
-        .attr('r', 4)
-        .style('fill', '#ffff00')
-        .attr('id', 'circle_' + creation)
-        .on('contextmenu', function (d, i) {
-            d3.event.preventDefault();
-           // react on right-clicking;
-           // removeCircle('circle_' + creation);
-            d3.selectAll('g #' + 'circle_' + creation).remove();
-        });
+            .attr('cx', xPosition)
+            .attr('cy', yPosition)
+            .attr('r', 4)
+            .style('fill', '#ffff00')
+            .attr('id', 'circle_' + creation)
+            .on('contextmenu', function (d, i) {
+                d3.event.preventDefault();
+                // react on right-clicking;
+                // removeCircle('circle_' + creation);
+                d3.selectAll('g #' + 'circle_' + creation).remove();
+            });
         
-        var svgTooltip = svgCircle.append('title')    // tooltip with circle x, y
+        var svgTooltip = svgCircle.append('title')    // tooltip - circle x, y
             .text(function() {
                 return xPosition + ', ' + yPosition;	  
             });
     });
+	
+
+    // add info panel
+    var panel = jQuery('#panel').show();
 }
 
 
+// @deprecated
 annotools.prototype.showDotTools = function() {
     
-	// redirect
+    // redirect
     window.location = 'http://129.49.249.191/camicroscope_alina/testaj/test.html';
 	
 }
