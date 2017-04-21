00e86c1ec17dbadaf53f9434db311c084b2b2605
bridge wang <tigerfan7495@gmail.com>
2017-04-21 13:20:25 -0400

    Update getMultipleAnnotsClone.php

--- api/Data/getMultipleAnnotsClone.php
+++ api/Data/getMultipleAnnotsClone.php
@@ -37,8 +37,8 @@ if (!empty($_SESSION['api_key'])) {
             
 
             $url = $getUrl . "&CaseId=" . $iid ."&x1=" . $x . "&y1=" . $y . "&x2=" . $x1 . "&y2=" . $y1 . "&footprint=" . $area . "&algorithms=" . $algorithms;
-            echo $url;
-		      	$getRequest = new RestRequest($url,'GET');
+            //echo $url;
+	    $getRequest = new RestRequest($url,'GET');
             $getRequest->execute();
             //print_r($url);            
             //print_r($getRequest);
