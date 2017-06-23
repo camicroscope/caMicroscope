7783a92031934c43fcfc49d9ae065f712d3dec1d
Ryan Birmingham <birm@rbirm.us>
2017-06-23 16:12:59 -0400

    re-add tissue id to url with state

--- osdCamicroscope.php
+++ osdCamicroscope.php
@@ -34,7 +34,7 @@
 	    <!--<script src="/featurescapeapps/js/findapi_config.js" type="text/javascript"></script>-->
         <script src="/js/config.js"></script>
 
-        <script src="js/openseadragon/openseadragon-bin-1.0.0/openseadragon.js"></script>
+        <script src="js/openseadragon/openseadragon-bin-2.0.0/openseadragon.js"></script>
         <script src="js/openseadragon/openseadragon-imaginghelper.min.js"></script>
         <script src="js/openseadragon/openseadragon-scalebar.js"></script>
         <script src="js/openseadragon/openseadragonzoomlevels.js"></script>
