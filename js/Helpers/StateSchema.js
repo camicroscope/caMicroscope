368205c9cf358d9e9b226094934c1b0c314b745d
Ryan Birmingham <birm@rbirm.us>
2017-11-10 18:36:11 -0500

    start integrating new state manager

---
+++ js/Helpers/StateSchema.js
@@ -0,0 +1,43 @@
+// this code helps us define our schema for state mangment
+// schema : {position:{x:x,y:y,z:z}, algs:[algname1,algname2]}
+var camic_state = new StateManager('state');
+
+function setPosition(position){
+  var pt = new OpenSeadragon.Point(position.x, position.y);
+  viewer.viewport.zoomTo(position.z, pt);
+  viewer.viewport.panTo(pt, true);
+}
+
+// TODO test if this method always works
+function setAlgs(algList){
+  SELECTED_ALGORITHM_LIST = algList;
+}
+
+// initalize after 500 ms
+setTimeout(function(){
+  camic_state.add_key('position', setPosition);
+  camic_state.add_key('alg', setAlgs);
+  // before touching the url, get what we already have
+  camic_state.initialize();
+  camic_state.clear_url();
+}, 500);
+
+
+algHandler = function() {
+  camic_state.alg=SELECTED_ALGORITHM_LIST;
+  camic_state.set_url();
+};
+moveHandler = function() {
+  var pos = viewer.viewport.getCenter(true);
+  var zoom = viewer.viewport.getZoom(true);
+  camic_state.zoom={'x':pos.x, 'y':pos.y, 'z':zoom};
+  camic_state.set_url();
+};
+
+// TODO make this actually trigger usefully
+// update url when requested only (this should be share)
+function onRequest(){
+  moveHandler();
+  algHandler();
+  alert(camic_state.encode(camic_state.vals));
+}
