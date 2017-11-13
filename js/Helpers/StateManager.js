156400ce5644747f13faa36f45504b506ffe8d63
Ryan Birmingham <birm@rbirm.us>
2017-11-13 11:34:38 -0500

    neaten history items

--- js/Helpers/StateManager.js
+++ js/Helpers/StateManager.js
@@ -8,7 +8,7 @@ class StateManager {
     constructor(prefix) {
         this.prefix = prefix;
         this.vals = {};
-        this.getters = {};
+        this.setters = {};
     }
 
     /* Add a key to the state variable */
@@ -19,7 +19,7 @@ class StateManager {
          * then use (this).vals['key'] to set key values
          * run (this).set_url();  to update the url with all set key values
          */
-        this.getters[name] = callback;
+        this.setters[name] = callback;
     }
 
     encode(state_object) {
@@ -31,19 +31,19 @@ class StateManager {
         return encodeURIComponent(btoa(JSON.stringify(state_object)));
     }
 
-    decode(encoded_string){
-      /* decoding for state from url
-       * @param encoded_string - the encoded string
-       * uses base64 decoding
-       * "exotic" objects likely won't work correctly without a way to convert to and from string
-       */
-      return JSON.parse(atob(decodeURIComponent(encoded_string)));
+    decode(encoded_string) {
+        /* decoding for state from url
+         * @param encoded_string - the encoded string
+         * uses base64 decoding
+         * "exotic" objects likely won't work correctly without a way to convert to and from string
+         */
+        return JSON.parse(atob(decodeURIComponent(encoded_string)));
     }
 
     set_url() {
-       /* Sets the current state into the url
-        * This updates the string to reflect all set (this).vals for all keys
-        */
+        /* Sets the current state into the url
+         * This updates the string to reflect all set (this).vals for all keys
+         */
         var state_string = this.encode(this.vals);
         // get all url components
         var previous = location.search.substring(1);
@@ -55,12 +55,12 @@ class StateManager {
         p_var[this.prefix] = state_string;
         // put paramater string back together, as modified
         var params = Object.keys(p_var).map((i) => i + '=' + p_var[i]).join('&');
-        window.history.replaceState("{}", "document.title", "?" + params);
+        window.history.replaceState({}, document.title,location.pathname + "?" + params);
     }
 
     get_url_state() {
-       /* fetches all state information from the current url
-        */
+        /* fetches all state information from the current url
+         */
         // get all url components
         var previous = location.search.substring(1);
         var p_var = previous ? JSON.parse('{"' + previous.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
@@ -73,36 +73,36 @@ class StateManager {
 
     clear_url() {
         /* remove this param from the url
-        */
+         */
         var previous = location.search.substring(1);
         var p_var = previous ? JSON.parse('{"' + previous.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
             function(key, value) {
                 return key === "" ? value : decodeURIComponent(value)
             }) : {}
         // pull out our state value
-        delete p_var['a'];
+        delete p_var[this.prefix];
         var params = Object.keys(p_var).map((i) => i + '=' + p_var[i]).join('&');
-        window.history.replaceState({}, document.title,
+        window.history.replaceState({}, document.title,location.pathname + "?" + params);
     }
 
     initialize(state) {
-      /* run all set functions based on the url and registry */
+        /* run all set functions based on the url and registry */
         for (var i in state) {
-            if (i in this.getters) {
-                this.getters[i](state[i]);
+            if (i in this.setters) {
+                this.setters[i](state[i]);
             }
         }
     }
 
-    to_storage(key){
+    to_storage(key) {
         /* copy state from url component to local storage
          * @param key - the key to store under localstorage
          */
         localStorage.setItem(key, this.get_url_state());
     }
 
-    from_storage(key){
-       /* copy state from local storage to url component
+    from_storage(key) {
+        /* copy state from local storage to url component
          * @param key - the key to retrieve under localstorage
          */
         return localStorage.getItem(key);
--- js/Helpers/StateSchema.js
+++ js/Helpers/StateSchema.js
@@ -13,24 +13,25 @@ function setAlgs(algList){
   SELECTED_ALGORITHM_LIST = algList;
 }
 
-// initalize after 500 ms
+// initalize after 500 seconds
 setTimeout(function(){
   camic_state.add_key('position', setPosition);
   camic_state.add_key('alg', setAlgs);
   // before touching the url, get what we already have
-  camic_state.initialize();
-  camic_state.clear_url();
+  camic_state.initialize(camic_state.decode(camic_state.get_url_state()));
+  //camic_state.clear_url();
 }, 500);
 
 
 algHandler = function() {
-  camic_state.alg=SELECTED_ALGORITHM_LIST;
+  camic_state.vals['alg']=SELECTED_ALGORITHM_LIST;
   camic_state.set_url();
 };
+
 moveHandler = function() {
   var pos = viewer.viewport.getCenter(true);
   var zoom = viewer.viewport.getZoom(true);
-  camic_state.zoom={'x':pos.x, 'y':pos.y, 'z':zoom};
+  camic_state.vals['position']={'x':pos.x, 'y':pos.y, 'z':zoom};
   camic_state.set_url();
 };
 
