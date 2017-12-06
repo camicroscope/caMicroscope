805cfb1ea9b9176ff7225a381bbbbf75c97ae006
Ryan Birmingham <birm@rbirm.us>
2017-12-06 17:31:28 -0500

    update doc for spyglass

--- js/Helpers/Spyglass.js
+++ js/Helpers/Spyglass.js
@@ -3,7 +3,7 @@ function Spyglass(viewer, modal_viewer) {
     /**
      * Creates a callback which starts single window magnifitation code
      * Include this file and start with something like:
-     * window.setTimeout(magnifier(viewer, modal_viewer),500);
+     * window.setTimeout(Spyglass(viewer, modal_viewer),500);
      */
     // return a callback
     return function() {
