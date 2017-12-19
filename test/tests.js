e38731785724dc2059ef2bb90142087295be00e1
Ryan Birmingham <birm@rbirm.us>
2017-12-19 12:57:45 -0500

    still failing tests, commiting before flight

--- test/tests.js
+++ test/tests.js
@@ -1,48 +1,63 @@
 var url = "http://localhost:3000/osdCamicroscope.php?tissueId=PC_052_0_1";
-casper.test.begin('Camic Frontend', 8, function suite(test) {
-    casper.start(url, function() {
+casper.test.begin('Camic Frontend', 7, function suite(test) {
+    casper.start(url).then(function() {
       // Image display
-      test.assert(canvas.getImageData(10, 10, 10, 10).data.reduce(function(a,b) {return a+b}));
+      test.assertEval(function(){
+        require('utils').dump(document.getElementsByTagName("canvas")[0].getContext('2d').getImageData(10, 10, 10, 10).data.reduce(function(a,b) {return a+b}))
+        return document.getElementsByTagName("canvas")[0].getContext('2d').getImageData(10, 10, 10, 10).data.reduce(function(a,b) {return a+b}) > 0;
+      });
+
       // Toolbar ok
-      test.assertEquals(document.getElementById('tool').style.height, "48px");
-      // OSD basic test
-      // State link looks OK
-      var pt = new OpenSeadragon.Point(0.1, 0.2);
-      viewer.viewport.panTo(pt, true);
-      viewer.viewport.zoomTo(3);
-      camic_state.set_url();
-      var state = camic_state.decode(camic_state.get_url_state());
-      // compare set values to state
-      // TODO these numbers are wrong!!!
-      test.assert((state.positon.x===0.1)&
-        (state.position.y===0.2)&
-        (state.position.z===4));
+      test.assertEval(function(){
+        return document.getElementById('tool').style.height == "48px"
+      });
+      // ~!!
+      // State link
+      test.assertEval(function(){
+        var pt = new OpenSeadragon.Point(0.1, 0.2);
+        viewer.viewport.panTo(pt, true);
+        viewer.viewport.zoomTo(3);
+        camic_state.set_url();
+        var state = camic_state.decode(camic_state.get_url_state());
+        // compare set values to state
+        // TODO these numbers are wrong!!!
+        return (state.positon.x===0.1)&
+          (state.position.y===0.2)&
+          (state.position.z===4)
+      });
+
       // Algorithm button
-      document.getElementsByClassName("toolButton")[3].click();
-      // Menu comes up
-      // NOTE: offsetParent returns null iff element is invisible
-      test.assert(!(document.getElementById('panel').offsetParent === null));
-      // Shows whatever I seed it with
-      // TODO
-      // Menu goes away
-      document.getElementsByClassName("toolButton")[3].click();
-      test.assert((document.getElementById('panel').offsetParent === null));
-      // Check case ID
-      // TODO
+      test.assertEval(function(){
+        document.getElementsByClassName("toolButton")[3].click();
+        // Menu comes up
+        // NOTE: offsetParent returns null iff element is invisible
+        return !(document.getElementById('panel').offsetParent === null)
+      });
+      // TODO Shows whatever I seed it with
+      test.assertEval(function(){
+        document.getElementsByClassName("toolButton")[3].click();
+        return (document.getElementById('panel').offsetParent === null)
+      });
+
+      // TODO Check case ID
 
       // magnifier is invisible initially
-      test.assert((document.getElementById('spyglass').offsetParent === null));
+      test.assertEval(function(){
+        (document.getElementById('spyglass').offsetParent === null)
+      });
       // magnifier toggles on button press
-      document.getElementsByClassName("toolButton")[3].click();
-      // magnifier displays ok
-      test.assert(!(document.getElementById('spyglass').offsetParent === null));
+      test.assertEval(function(){
+        document.getElementsByClassName("toolButton")[3].click();
+        return !(document.getElementById('spyglass').offsetParent === null)
+      });
       // magnifier seems to follow mouse
       this.mouse.move(400, 300);
       var pos1 = document.getElementById('spyglass').getBoundingClientRect();
       this.mouse.move(100, 100);
       var pos2 = document.getElementById('spyglass').getBoundingClientRect();
       test.assert(pos1.bottom !== pos2.bottom);
-
-      test.done();
+    });
+    casper.run(function() {
+        test.done();
     });
 });
