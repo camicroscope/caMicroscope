6c10607eab7ec1bb3ee640a95e6bb1bfa1a6b0c2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-01-12 09:49:52 -0500

    one line

--- osdCamicroscope.php
+++ osdCamicroscope.php
@@ -166,8 +166,7 @@ include 'shared/osdHeader.php';
         /*Pan and zoom to point*/
         var bound_x = <?php echo json_encode($_GET['x']); ?>;
         var bound_y = <?php echo json_encode($_GET['y']); ?>;
-        var zoom = <?php echo json_encode($_GET['zoom']); ?> ||
-        viewer.viewport.getMaxZoom();
+        var zoom = <?php echo json_encode($_GET['zoom']); ?> || viewer.viewport.getMaxZoom();
         zoom = Number(zoom); // convert string to number if zoom is string
 
         /*
