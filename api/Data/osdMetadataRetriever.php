d5cad30e6888acf1eac9d2013c670ae728c6dacd
bridge wang <tigerfan7495@gmail.com>
2018-02-01 18:13:34 -0500

    Update osdMetadataRetriever.php

--- api/Data/osdMetadataRetriever.php
+++ api/Data/osdMetadataRetriever.php
@@ -5,10 +5,12 @@ include_once("CamicUtils.php");
 $utils = new CamicUtils($_SESSION);
 $tissueId = $_GET['imageId'];
 $fileLocation = $utils->retrieveImageLocation($tissueId);
+$imageStatus = $utils->retrieveImageStatus($tissueId);
+$assignTo = $utils->retrieveImageAssignTo($tissueId);
 #$dzi = $utils->setUpSymLinks($fileLocation);
 $dzi = $utils->setUpSVSImage($fileLocation);
 $finalMpp = $utils->retrieveMpp($tissueId);
 $returnArray = array();
-array_push($returnArray,$finalMpp,$dzi);
+array_push($returnArray,$finalMpp,$dzi,$imageStatus,$assignTo);
 echo json_encode($returnArray);
 ?>
