508626c455b73df8f719b70c002875997ec47400
Ryan Birmingham <birm@rbirm.us>
2018-02-02 21:16:56 -0500

    shorten message for session exp

--- js/Helpers/session_notify.js
+++ js/Helpers/session_notify.js
@@ -3,7 +3,7 @@ window.setInterval(function(){
     var rsp = JSON.parse(this.responseText)
     console.log(rsp);
     if (rsp.now - rsp.issued > 60*50){
-      window.alert("Your session is about to expire (within 10 minutes), act or be logged out. (Message Pending)")
+      window.alert("Your session has expired.")
     }
   }
   var oReq = new XMLHttpRequest();
