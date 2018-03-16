43169ea9ec276df3378500f6653ecedf5ab66ac2
Ryan Birmingham <birm@rbirm.us>
2018-03-16 15:34:04 -0400

    what happens if state happens immediately

--- js/Helpers/StateSchema.js
+++ js/Helpers/StateSchema.js
@@ -14,21 +14,17 @@ function setAlgs(algList){
 }
 
 // initalize after 500 mseconds
-document.addEventListener("ready-for-state",function(){
-  camic_state.add_key('position', setPosition);
-  camic_state.add_key('alg', setAlgs);
-  // before touching the url, get what we already have
-  try{
-    camic_state.initialize(camic_state.decode(camic_state.get_url_state()));
-  }
-  catch(e){
-    console.log(e);
-  }
-  // remove state url when done
-  // setTimeout(function(){
-  //   camic_state.clear_url();
-  // }, 500);
-});
+//document.addEventListener("ready-for-state",function(){
+camic_state.add_key('position', setPosition);
+camic_state.add_key('alg', setAlgs);
+// before touching the url, get what we already have
+try{
+  camic_state.initialize(camic_state.decode(camic_state.get_url_state()));
+}
+catch(e){
+  console.log(e);
+}
+//});
 
 
 algHandler = function() {
