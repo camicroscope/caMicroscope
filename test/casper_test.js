080d36a1940fca77349f8596f3c5eccbbf8393b0
Ryan Birmingham <birm@rbirm.us>
2018-03-20 16:42:57 -0400

    basic working test

--- test/casper_test.js
+++ test/casper_test.js
@@ -1,21 +1,10 @@
-casper.test.begin('Image loads okay', 5, function suite(test) {
+casper.test.begin('CAMICROSOPE repo test', 1, function suite(test) {
     casper.start("http://localhost:3000/osdCamicroscope.php?tissueId=TESTING", function() {
         test.assertExists('#tool', "toolbar present");
-        test.assertExists('img[title="Filter Markups"]', "Markups btn present");
-        test.click('img[title="Filter Markups"]');
-    });
-
-    casper.then(function() {
-        // select some algorithms
-        test.assertExists(".algorithmCheckbox[value=0]", "At least one alg checkbox present");
-        test.click(".algorithmCheckbox[value=0]");
-    });
-
-    casper.then(function() {
-        // check SELECTED_ALGORITHM_LIST
-        test.evaluateOrDie(function() {
-          return SELECTED_ALGORITHM_LIST.indexOf("wsi:r0.6:w0.8:l3:u200:k20:j0") >= 0
-        }, "algorithim selection failed")
+        this.evaluateOrDie(function() {
+          viewer.viewport.zoomTo(5);
+          return viewer.viewport.getZoom() == 5
+        }, "Viewer load fail")
     });
 
     casper.run(function() {
