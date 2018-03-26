c6fa689105b3f52b1513e1fa85544a528dee8a44
Ryan Birmingham <birm@rbirm.us>
2018-03-26 11:13:44 -0400

    don't bother instances where security disabled

--- js/Helpers/session_notify.js
+++ js/Helpers/session_notify.js
@@ -2,7 +2,7 @@ window.setInterval(function(){
   function reqListener () {
     var rsp = JSON.parse(this.responseText)
     console.log(rsp);
-    if (rsp.now - rsp.issued > 60*50){
+    if (rsp.issued && (rsp.now - rsp.issued > 60*50)){
       window.alert("Your session has expired.")
     }
   }
