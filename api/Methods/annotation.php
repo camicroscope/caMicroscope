84af31ce215a367f029b5c02542f8b2aa8f20013
Ryan Birmingham <birm@rbirm.us>
2018-03-28 14:22:37 -0400

    identify methods to reimplement

--- api/Methods/annotation.php
+++ api/Methods/annotation.php
@@ -3,7 +3,16 @@
 // bindaas requirements:
 // * ??
 $services = require 'config.php';
+$annotationsUrl = $services['annotations'];
 // GET
+"$annotationsUrl/MarkupsForImages/query/MarkupsAvilableForImage?"
+"$annotationsUrl/MarkupLoader/query/getMultipleMarkups?"
+"$annotationsUrl/MarkupLoader/query/getROI"
+
 // POST
+"$annotationsUrl/MarkupLoader/submit/json"
+"$annotationsUrl/MarkupsForImages/submit/json"
+
 // DELETE
+"$annotationsUrl/MarkupLoader/delete/deleteMultipleMarkups",
 ?>
