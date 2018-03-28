b4f853da5db1b2b96cc46cd5cf06c7c0329a89a3
Ryan Birmingham <birm@rbirm.us>
2018-03-28 13:52:20 -0400

    add simplified config

--- api/Methods/annotation.php
+++ api/Methods/annotation.php
@@ -1,4 +1,7 @@
 <?php require '../../../authenticate.php';
 // annotation handles all interactions with annotations, GET, POST, or DELETE
-
+$services = require 'config.php';
+// GET
+// POST
+// DELETE
 ?>
