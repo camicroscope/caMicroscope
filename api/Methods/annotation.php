d0b282922b8b23ec401ff90899b51d14dbc5e32d
Ryan Birmingham <birm@rbirm.us>
2018-03-28 17:15:57 -0400

    annotation boilerplate

--- api/Methods/annotation.php
+++ api/Methods/annotation.php
@@ -1,18 +1,60 @@
 <?php require '../../../authenticate.php';
 // annotation handles all interactions with annotations, GET, POST, or DELETE
 // bindaas requirements:
-// * ??
+// * MarkupsForImages
+// * MarkupLoader
+
 $services = require 'config.php';
 $annotationsUrl = $services['annotations'];
-// GET
-"$annotationsUrl/MarkupsForImages/query/MarkupsAvilableForImage?"
-"$annotationsUrl/MarkupLoader/query/getMultipleMarkups?"
-"$annotationsUrl/MarkupLoader/query/getROI"
 
-// POST
-"$annotationsUrl/MarkupLoader/submit/json"
-"$annotationsUrl/MarkupsForImages/submit/json"
+// require api key is set
+if (!empty($_SESSION['api_key'])) {
+    $api_key = $_SESSION['api_key'];
+} else {
+  die();
+}
 
-// DELETE
-"$annotationsUrl/MarkupLoader/delete/deleteMultipleMarkups",
+// reuire request type
+if(isset($_GET["type"])) {
+    $type = $_GET["type"];
+} else {
+  die();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "GET"){
+  // input - image id
+  if ($type == "algorithms"){
+    "$annotationsUrl/MarkupsForImages/query/MarkupsAvilableForImage?"
+  }
+  // input - image id
+  elseif ($type == "rois"){
+    "$annotationsUrl/MarkupLoader/query/getROI"
+  }
+  // input - view bounds, algorithm, footprint
+  elseif ($type == "markups"){
+    "$annotationsUrl/MarkupLoader/query/getMultipleMarkups?"
+  }
+}
+
+
+elseif ($_SERVER['REQUEST_METHOD'] == "POST"){
+  // input - annotation list
+  if ($type == "algorithms"){
+    "$annotationsUrl/MarkupsForImages/submit/json"
+  }
+  // input - annotation authorship
+  elseif ($type == "markups"){
+    "$annotationsUrl/MarkupLoader/submit/json"
+  }
+}
+
+elseif ($_SERVER['REQUEST_METHOD'] == "DELETE"){
+  elseif ($type == "markups"){
+    "$annotationsUrl/MarkupLoader/delete/deleteMultipleMarkups"
+  }
+}
+
+else {
+  die();
+}
 ?>
