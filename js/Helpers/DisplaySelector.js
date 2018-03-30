90edc344d5391f7970827c5bcb5fa0651f196709
Ryan Birmingham <birm@rbirm.us>
2018-03-30 19:48:04 -0400

    fix custom event detail

--- js/Helpers/DisplaySelector.js
+++ js/Helpers/DisplaySelector.js
@@ -2,18 +2,18 @@
 // such as side split or spyglass
 
 
-
 function overlay_url(id){
   return "api/Data/getAlgorithmsForImage.php?" + id;
 }
 
-let menu = document.createElement("div");
+var menu = document.createElement("div");
 menu.style.zIndex = 3;
 menu.style.display = "none";
 menu.id = "second_display_menu"
-let lst = document.createElement("ul");
+var lst = document.createElement("ul");
 lst.id = "second_display_list";
 menu.appendChild(lst);
+document.body.appendChild(menu);
 
 function show_images(img_api_url, lst){
   fetch(img_api_url)
@@ -25,10 +25,19 @@ function show_images(img_api_url, lst){
       d.forEach(function(item){
         let elem = document.createElement("li");
         elem.innerHtml = item.name;
-        elem.onclick = () => (document.dispatchEvent(new CustomEvent("SecImageLoad", {url: item.url, id: item.name})));
+        elem.onclick = () => (document.dispatchEvent(new CustomEvent("SecImageLoad", {detail: {url: item.url, id: item.name}})));
         lst.appendChild(elem);
       })
     });
+}
+
+function show_images_lst(d, lst){
+  d.forEach(function(item){
+    let elem = document.createElement("li");
+    elem.innerHtml = item.name;
+    elem.onclick = () => (document.dispatchEvent(new CustomEvent("SecImageLoad", {detail:{url: item.url, id: item.name}})));
+    lst.appendChild(elem);
+  })
 }
 
 function show_overlays(ovr_api_url, lst){
@@ -41,35 +50,43 @@ function show_overlays(ovr_api_url, lst){
       d.forEach(function(item){
         let elem = document.createElement("li");
         elem.innerHtml = item.name;
-        elem.onclick = () => (document.dispatchEvent(new CustomEvent("SecOverlay", {algs: [item.name]})));
+        elem.onclick = () => (document.dispatchEvent(new CustomEvent("SecOverlay", {detail: {algs: [item.name]}})));
         lst.appendChild(elem);
       })
     });
 }
 
 // register this event to pusing a button which requiees it
-document.addEventListner('display_select', function(detail){
+window.addEventListener('display_select', function(detail){
   var menu = document.getElementById("second_display_menu");
   var lst = document.getElementById("second_display_list");
   menu.style.display = "block";
-  if (detail.type == "img"){
-    show_images(detail.img_api_url, lst);
-  } else if (detail.type == "mkp"){
-    show_mkp(detail.mkp_api_url, lst);
+  if (detail.detail.type == "lst_img"){
+    show_images_lst(detail.detail.list, lst);
+  }
+  else if (detail.detail.type == "img"){
+    show_images(detail.detail.api_url, lst);
+  } else if (detail.detail.ype == "mkp"){
+    show_mkp(detail.detail.api_url, lst);
   } else {
     console.warn("unkown display selector requested");
   }
 })
 
 // event listner for img load
-document.addEventListner("SecImageLoad", function(detail){
-  viewer2.open(detail.url);
-  show_overlays(overlay_url(detail.id), document.getElementById("second_display_list"))
+window.addEventListener("SecImageLoad", function(detail){
+  viewer2.open(detail.detail.url);
+  show_overlays(overlay_url(detail.detail.id), document.getElementById("second_display_list"))
 });
 
 // event listner for mkp load
-document.addEventListner("SecOverlay", function(detail){
-  annotools2.algorithmList = detail.algs;
+window.addEventListener("SecOverlay", function(detail){
+  annotools2.algorithmList = detail.detail.algs;
   // close menu
   document.getElementById("second_display_menu").style.display = "none";
 });
+
+function start(){
+  e = new CustomEvent("display_select", {detail: {type: "lst_img", list: [{name: "first", url:"first"}, {name: "first", url:"first"}]}})
+  window.dispatchEvent(e);
+}
