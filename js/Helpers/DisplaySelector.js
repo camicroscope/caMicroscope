2b7aa91f8fbce710112adb329d71bc20b8aaa2cd
Ryan Birmingham <birm@rbirm.us>
2018-03-30 14:47:00 -0400

    plan for display selector

---
+++ js/Helpers/DisplaySelector.js
@@ -0,0 +1,14 @@
+//This file creates an overlay which aims to provide information to any second viewer
+// such as side split or spyglass
+
+// init
+// base image
+// target (to send img url and markups)
+// selecting img and mkp or just mkp?
+// where to get list of img? if applic
+// where to get list of markup
+
+// render
+// list of images if applic
+// list of markups, including none
+// pass selection to target
