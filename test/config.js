edd99819a60d02f2dbc34b4ac338b46d56daf26d
nanli <nan.li@emory.edu>
2018-09-13 13:14:22 -0400

    1. add global.origin and global.testFolder for config the test path. 2. enhance the test cases for messagequeue and camessage.

---
+++ test/config.js
@@ -0,0 +1,5 @@
+//config.js
+//
+//global.origin = 'http://localhost:4010'
+global.origin = 'http://127.0.0.1:8080'
+global.testFolder = '/test'
--- test/ui/camessage-test-cases.js
+++ test/ui/camessage-test-cases.js
@@ -5,37 +5,40 @@ const { JSDOM } = jsdom;
 // jsdom instance
 let dom;
 
-// read test page
- JSDOM.fromURL(
-    'http://127.0.0.1:8080/test/ui/camessage-test-cases.html', 
-    {
-      runScripts: "dangerously",
-      resources: "usable"
-    }
-  ).then(jsdom => {
-  	dom = jsdom;
-  	// check the loading flag
-	let checkLoaded = setInterval(function(){
-		if(jsdom.window.document.getElementById('isLoad').checked){
-
-			clearInterval(checkLoaded);
-			//console.count('start running test cases');
-			// run test cases if page is ready. 
-			run();
-		}
-	},100);
-  }).catch(function(error) {
-    console.log(error);
-  })
-
-// 
 describe('Ca Message Component', function () {
+	
+	/* --  prepare test START -- */
+	// loading the test page and wait til the scripts on the test pages are executed.
+	before(function(done){
+		
+		// load test page
+		 JSDOM.fromURL(
+		    `${global.origin}${global.testFolder}/ui/camessage-test-cases.html`, 
+		    {
+		      runScripts: "dangerously",
+		      resources: "usable"
+		    }
+		  ).then(jsdom => {
+		  	dom = jsdom;
+		  	// check the loading flag, waiting the scripts executed
+			let checkLoaded = setInterval(function(){
+				if(jsdom.window.document.getElementById('isLoad').checked){
+					clearInterval(checkLoaded);
+					// run test cases if page is ready. 
+					done();
+				}
+			},100);
+		  }).catch(function(error) {
+		  	// error
+		    done(error);
+		  })		
+	});
+	/* --  prepare test END -- */
 
 	// case1: initialize 
-	it('Initialize', function () { 
+	it('Initialize', function () {
 		const case1 = dom.window.document.getElementById('case1');
 		assert.equal(case1.textContent,'Hello World');
-
 	});
 
 	// case 2: change text
