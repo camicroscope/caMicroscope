1fff52b45285ee94a1068d5f6bb22d23962d3b48
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2018-10-04 16:39:29 -0400

    Fixed Type conversion of strings to integer

--- api/Data/getAnnotSpatial.php
+++ api/Data/getAnnotSpatial.php
@@ -65,7 +65,7 @@ switch ($_SERVER['REQUEST_METHOD'])
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($annotationList, JSON_NUMERIC_CHECK));
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($annotationList));
         $result = curl_exec($ch);
         if($result === false){
             $result =  curl_error($ch);
