2d3473a14efb5ee83c994e5a4d914a9a0686922b
Ryan Birmingham <birm@rbirm.us>
2018-11-05 14:00:59 -0500

    try to fix reported package issue

--- package/DummyStore.js
+++ package/DummyStore.js
@@ -2,9 +2,9 @@ function DummyStore(){
   console.log("this has run")
   Store.prototype.findSlideDummy = function(){
     return new Promise(function(res,rej){
-      let data = [{ "_id" : { "$oid" : "5bb6861db8bb11ffeaa05b6d"} , "name" : "CMU1" , "location" : "/images/sample.svs" , "mpp" : 0.499}]
-      let x = {'json': x=>data}
-      res(x)
+      res({'json': function(){
+        return [{ "_id" : { "$oid" : "5bb6861db8bb11ffeaa05b6d"} , "name" : "CMU1" , "location" : "/images/sample.svs" , "mpp" : 0.499}]
+      }})
     })
   }
 }
