31c686b7ca1af3428f978f2d94e525d7cd615829
Ryan Birmingham <birm@rbirm.us>
2018-11-05 10:23:51 -0500

    need to 'let' them be

--- package/DummyStore.js
+++ package/DummyStore.js
@@ -2,8 +2,8 @@ function DummyStore(){
   console.log("this has run")
   Store.prototype.findSlideDummy = function(){
     return new Promise(function(res,rej){
-      data = [{ "_id" : { "$oid" : "5bb6861db8bb11ffeaa05b6d"} , "name" : "CMU1" , "location" : "/images/sample.svs" , "mpp" : 0.499}]
-      x = {'json': x=>data}
+      let data = [{ "_id" : { "$oid" : "5bb6861db8bb11ffeaa05b6d"} , "name" : "CMU1" , "location" : "/images/sample.svs" , "mpp" : 0.499}]
+      let x = {'json': x=>data}
       res(x)
     })
   }
