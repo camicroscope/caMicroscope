3ef29136c8cac495f4d1fe0d016d7507299a42cb
Ryan Birmingham <birm@rbirm.us>
2018-12-18 19:44:55 -0500

    remove unneeded calls, add warning

--- package/ImgBoxMods.js
+++ package/ImgBoxMods.js
@@ -2,46 +2,34 @@ function ImgBoxMods(){
   console.warn("{imgbox mods enabled}")
   CaMic.prototype.default_loadImg = Camic.prototype.loadImg
   CaMic.prototype.loadImg = function(func){
-    // loads current image
-    // if id is set, use id
-    var slidePromise;
-    if(this.slideQuery.hasOwnProperty('id') && this.slideQuery.id){
-      slidePromise = this.store.getSlide(this.slideQuery.id)
-    }
-    else {
-      slidePromise = this.store.findSlide(this.slideQuery.name, this.slideQuery.location)
-    }
-    slidePromise
-      .then((x)=>{
-        if(!x || !OpenSeadragon.isArray(x) || !x.length || !x[0].location){
-          redirect($D.pages.table,`Can't find the slide information`);
-          return;
-        }
-        this.slideId = x[0]["_id"]["$oid"]
-        this.slideName = x[0]['name']
-        this.study = x[0]['study']
-        this.specimen = x[0]['study']
-        this.viewer.open("./img/Slide/"+ x[0]["_id"]["$oid"] + ".dzi");
-        // set scalebar
-        this.createScalebar(x[0].mpp)
-        var imagingHelper = new OpenSeadragonImaging.ImagingHelper({
-          viewer: this.viewer
-        });
+    let params = new URLSearchParams(document.location.search.substring(1));
+    let slideId = params.get("id");
+    this.slideId = x[0]["_id"]["$oid"]
+    this.slideName = "local"
+    this.study = ""
+    this.specimen = ""
+    let ibmox_url = decodeURIComponent(slideId)
+    this.viewer.open(ibmox_url);
+    // set scalebar
+    //his.createScalebar(x[0].mpp)
+    var imagingHelper = new OpenSeadragonImaging.ImagingHelper({
+      viewer: this.viewer
+    });
 
-        imagingHelper.setMaxZoom(1);
-        if(func && typeof func === 'function') func.call(null,x[0]);
-        Loading.text.textContent = `loading slide's tiles...`;
-        this.mpp = x[0].mpp;
-
-
-      })
-      .catch(e=>{
-        Loading.close();
-        $UI.message.addError('loadImg Error');
-        console.error(e);
-
-        if(func && typeof func === 'function') func.call(null,{hasError:true,message:e});
-      })
+    imagingHelper.setMaxZoom(1);
+    // create item to pass to the callback function, previously x[0] (slide data)
+    let x = {}
+    x['_id']
+    x.name = this.slideName
+    x.study = this.study
+    x.specimen = this.specimen
+    x.mpp = 0;
+    x.location = ibmox_url;
+    if(func && typeof func === 'function') func.call(null,x);
+    Loading.text.textContent = `loading slide's tiles...`;
+    this.mpp = 0;
+    // WARN; note that spyglass isn't working due to semi-hardcoded value at init.js line 140
+    // we may want another init.js or our own callback
   }
 }
 
