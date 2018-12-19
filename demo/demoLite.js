03dff5a15c010a4e9b80a9dbff7bb83bf5e06f80
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-12-19 15:45:46 -0500

    draw rectangle selection

--- demo/demoLite.js
+++ demo/demoLite.js
@@ -1,130 +1,154 @@
-/*
-Code borrowed from init.js
-Create a line-draw button.
-To check your drawing, use $CAMIC.viewer.canvasDrawInstance.getImageFeatureCollection()
-Call code from copy of demo.html
-<script src='./js/demoLite.js'></script>
-*/
-
-// CAMIC is an instance of camicroscope core
 let $CAMIC = null;
-
-// for all instances of UI components
 const $UI = {};
-
 const $D = {
-    pages: {
-        home: './table.html',
-        table: './table.html'
-    },
-    params: null // parameter from url - slide Id and status in it (object).
+  pages: {
+    home: './table.html',
+    table: './table.html'
+  },
+  params: null // parameter from url - slide Id and status in it (object).
 };
 
-// initialize viewer page
-function initialize() {
-    // init UI -- some of them need to wait data loader to load data
-    // because UI components need data to initialize
-    initUIcomponents();
+function drawRectangle(note) {
+  console.log(note);
 
-    // create a viewer and set up
-    initCore();
+  let canvas = $CAMIC.viewer.drawer.canvas;
+  //$CAMIC.viewer.canvas.querySelector('canvas')
+
+  canvas.style.cursor = note.checked ? 'crosshair' : 'default';
+
+  if (note.checked)
+  {
+    initDraw(canvas);
+  }
+  else
+  {
+    stopDraw(canvas);
+  }
+
+}
+
+function stopDraw(canvas)
+{
+  // idk yet.
+}
+
+function initDraw(canvas) {
+  var mouse = {
+    x: 0,
+    y: 0,
+    startX: 0,
+    startY: 0
+  };
+
+  function setMousePosition(e) {
+    var ev = e || window.event; //Moz || IE
+    if (ev.pageX) { //Moz
+      mouse.x = ev.pageX + window.pageXOffset;
+      mouse.y = ev.pageY + window.pageYOffset;
+    } else if (ev.clientX) { //IE
+      mouse.x = ev.clientX + document.body.scrollLeft;
+      mouse.y = ev.clientY + document.body.scrollTop;
+    }
+  }
+  var element = null;
+  canvas.onmousemove = function (e) {
+    setMousePosition(e);
+    if (element !== null) {
+      element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
+      element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+      element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
+      element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+    }
+  };
+
+  canvas.onclick = function (e) {
+    console.log('click', e);
+
+    if (element !== null) {
+      element = null;
+      canvas.style.cursor = "default";
+      console.log("finished.");
+    } else {
+      console.log("begun.");
+      mouse.startX = mouse.x;
+      mouse.startY = mouse.y;
+      element = document.createElement('div');
+      element.className = 'rectangle';
+      element.style.left = mouse.x + 'px';
+      element.style.top = mouse.y + 'px';
+      canvas.appendChild(element);
+      canvas.style.cursor = "crosshair";
+    }
+  }
+}
+
+function initialize() {
+  initUIcomponents();
+  initCore();
+}
+
+function initUIcomponents() {
+  $UI.toolbar = new CaToolbar({
+    id: 'ca_tools',
+    zIndex: 601,
+    hasMainTools: false,
+    subTools: [
+      // rectangle draw
+      {
+        icon: 'timeline',
+        type: 'check',
+        value: 'rect',
+        title: 'Rectangle',
+        callback: drawRectangle
+      }
+    ]
+  });
 }
 
 // setting core functionality
 function initCore() {
-    // start initial
-
-    const opt = {
-        hasZoomControl: true,
-        hasDrawLayer: true,
-        hasLayerManager: true,
-        hasScalebar: true,
-        hasMeasurementTool: true
-    };
-
-    // set states if exist
-    if ($D.params.states) {
-        opt.states = $D.params.states;
+  // start initial
+  const opt = {
+    hasZoomControl: true,
+    hasDrawLayer: true,
+    hasLayerManager: true,
+    hasScalebar: true,
+    hasMeasurementTool: true
+  };
+  // set states if exist
+  if ($D.params.states) {
+    opt.states = $D.params.states;
+  }
+  try {
+    $CAMIC = new CaMic("main_viewer", $D.params.slideId, opt);
+  } catch (error) {
+    Loading.close();
+    $UI.message.addError('Core Initialization Failed');
+    console.error(error);
+    return;
+  }
+  $CAMIC.loadImg(function (e) {
+    // image loaded
+    if (e.hasError) {
+      $UI.message.addError(e.message)
     }
-
-    try {
-        $CAMIC = new CaMic("main_viewer", $D.params.slideId, opt);
-    } catch (error) {
-        Loading.close();
-        $UI.message.addError('Core Initialization Failed');
-        console.error(error);
-        return;
-    }
-
-    $CAMIC.loadImg(function (e) {
-        // image loaded
-        if (e.hasError) {
-            $UI.message.addError(e.message)
-        }
-    });
-
-    // draw something
-    // $CAMIC.viewer.addOnceHandler('open', function (e) {
-    //     // ready to draw
-    //     console.log($CAMIC.viewer.omanager);
-    //
-    // });
+  });
 }
 
-function initUIcomponents() {
-    // ui init
-    $UI.toolbar = new CaToolbar({
-        /* opts that need to think of */
-        id: 'ca_tools',
-        zIndex: 601,
-        hasMainTools: false,
-        subTools: [
-            // free-line
-            {
-                icon: 'border_color',// material icons' name
-                title: 'Line',
-                type: 'check',
-                value: 'line',
-                callback: freeLine
-            }
-
-        ]
-    });
-
-}
-
-// pen draw callback
-function freeLine(e) {
-    if (!$CAMIC.viewer.canvasDrawInstance) {
-        alert('draw doesn\'t initialize');
-        return;
+function redirect(url ,text = '', sec = 5){
+  let timer = sec;
+  setInterval(function(){
+    if(!timer) {
+      window.location.href = url;
     }
-    console.log(e);
-    const canvasDraw = $CAMIC.viewer.canvasDrawInstance;
-    canvasDraw.drawMode = 'line';
-    if (e.checked) {
-        canvasDraw.drawOn();
-    } else {
-        canvasDraw.drawOff();
+
+    if(Loading.instance.parentNode){
+      Loading.text.textContent = `${text} ${timer}s.`;
+    }else{
+      Loading.open(document.body,`${text} ${timer}s.`);
     }
+    // Hint Message for clients that page is going to redirect to Flex table in 5s
+    timer--;
+
+  }, 1000);
 }
-
-function redirect(url, text = '', sec = 5) {
-    console.log('we are here');
-    let timer = sec;
-    setInterval(function () {
-        if (!timer) {
-            window.location.href = url;
-        }
-
-        if (Loading.instance.parentNode) {
-            Loading.text.textContent = `${text} ${timer}s.`;
-        } else {
-            Loading.open(document.body, `${text} ${timer}s.`);
-        }
-        // Hint Message for clients that page is going to redirect to Flex table in 5s
-        timer--;
-
-    }, 1000);
-}
-
