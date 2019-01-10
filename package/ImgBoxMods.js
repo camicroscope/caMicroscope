076596d8c877d4855a5b7f9ade432f5628d80674
Ryan Birmingham <birm@rbirm.us>
2019-01-09 19:50:48 -0500

    align with erichs method

--- package/ImgBoxMods.js
+++ package/ImgBoxMods.js
@@ -1,36 +1,55 @@
-function ImgBoxMods(){
+function ImgBoxMods() {
   console.warn("{imgbox mods enabled}")
   CaMic.prototype.default_loadImg = Camic.prototype.loadImg
-  CaMic.prototype.loadImg = function(func){
-    let params = new URLSearchParams(document.location.search.substring(1));
-    let slideId = params.get("id");
-    this.slideId = x[0]["_id"]["$oid"]
+  CaMic.prototype.loadImg = function(func) {
+    var urlParams = new URLSearchParams(window.location.search);
+    var p = urlParams.get('id');
+    console.log("image ID : " + p);
+    let slideId = p
+    this.slideId = slideId
     this.slideName = "local"
     this.study = ""
     this.specimen = ""
-    let ibmox_url = decodeURIComponent(slideId)
-    this.viewer.open(ibmox_url);
-    // set scalebar
-    //his.createScalebar(x[0].mpp)
-    var imagingHelper = new OpenSeadragonImaging.ImagingHelper({
-      viewer: this.viewer
+    fetch(p + "/info.json").then(response => {
+      return response.json();
+    }).then(data => {
+      let ibmox_source = {
+        "@context": "http://iiif.io/api/image/2/context.json",
+        //"@id": "/iiif?iri=https://s3.amazonaws.com/ebremeribox/TCGA-02-0001-01C-01-BS1.0cc8ca55-d024-440c-a4f0-01cf5b3af861.svs",
+        "@id": p,
+        "height": data.height,
+        "width": data.width,
+        "profile": ["http://iiif.io/api/image/2/level2.json"],
+        "protocol": "http://iiif.io/api/image",
+        "tiles": [{
+          "scaleFactors": [1, 2, 4, 8, 16, 32],
+          "width": 256
+        }]
+      }
+      this.viewer.open(ibmox_source);
+      // set scalebar
+      //his.createScalebar(x[0].mpp)
+      var imagingHelper = new OpenSeadragonImaging.ImagingHelper({
+        viewer: this.viewer
+      });
+
+      imagingHelper.setMaxZoom(1);
+      // create item to pass to the callback function, previously x[0] (slide data)
+      let x = {}
+      x['_id']
+      x.name = this.slideName
+      x.study = this.study
+      x.specimen = this.specimen
+      x.mpp = 0;
+      x.location = ibmox_url;
+      x.url = ibmox_url
+      if (func && typeof func === 'function') func.call(null, [x]);
+      Loading.text.textContent = `loading slide's tiles...`;
+      this.mpp = 0;
+      // WARN; note that spyglass isn't working due to semi-hardcoded value at init.js line 140
+      // we may want another init.js or our own callback
     });
 
-    imagingHelper.setMaxZoom(1);
-    // create item to pass to the callback function, previously x[0] (slide data)
-    let x = {}
-    x['_id']
-    x.name = this.slideName
-    x.study = this.study
-    x.specimen = this.specimen
-    x.mpp = 0;
-    x.location = ibmox_url;
-    x.url = ibmox_url
-    if(func && typeof func === 'function') func.call(null,[x]);
-    Loading.text.textContent = `loading slide's tiles...`;
-    this.mpp = 0;
-    // WARN; note that spyglass isn't working due to semi-hardcoded value at init.js line 140
-    // we may want another init.js or our own callback
   }
 }
 
