4e3c1f8721e22505b24599a26638c5102227d73e
nanli <nan.li@emory.edu>
2019-01-24 16:30:54 -0500

    fixed the travis test error. Add test for toolbars

--- test/ui/toolbar-test-cases.js
+++ test/ui/toolbar-test-cases.js
@@ -37,101 +37,114 @@ describe('Toolbar Component', function () {
 
 
 	/* -- test cases START -- */
-	// describe('constructor:options[default]', function () {
+	describe('constructor:options[default]', function () {
+		// menu is close
+		
+			it('style', function () {
+				//const tools = ;
+				let list = dom.window.tools.elt.querySelectorAll('li[class]');
+				//console.log(list[0].classList)
+				//const menu = dom.window.document.getElementById('menu1');
+				assert.equal(list[0].classList.contains('separator'), true);
+				assert.equal(list[1].classList.contains('handler'), true);
+				list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				assert.equal(list.length, 6);
+			});
 
-	// 	// menu is close
-	// 	it('menu close', function () {
-	// 		const menu = dom.window.document.getElementById('menu1');
-	// 		assert.equal(menu.style.width, '0px');
-	// 	});
-	// });
+			it('li > states', function () {
+				// status of check/btn				
+				dom.window.tools.elt.querySelectorAll(' .tools > li:not([class]) > input[type=checkbox]').forEach(chk=>{
+					assert.equal(chk.checked, false);
+				});
+			});
+			
+			it('handler events', function() {
+				const handler = dom.window.tools.elt.querySelector('.handler > label');
+				// click first then all `li`s hide
+				eventFire(handler,'click', dom);
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				assert.equal(list[0].style.display,'');
+				assert.equal(list[1].style.display,'');
+				assert.equal(list[2].style.display,'none');
+				assert.equal(list[3].style.display,'none');
+				assert.equal(list[4].style.display,'none');
+				assert.equal(list[5].style.display,'none');
 
-	// describe('constructor:options(isOpen:true,width:500px)', function () {
+				// click first then a;; `li`s diplay
+				eventFire(handler,'click', dom);	
+				dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])').forEach(li=>{
+					assert.equal('','');
+				});
 
-	// 	// menu is open and 
-	// 	it('menu open and width is 500px', function () {
-	// 		const menu = dom.window.document.getElementById('menu2');
-	// 		assert.equal(menu.style.width, '500px');
-	// 	});
+			});
 
-	// });
+	});
 
+	describe('click events for each tool', function () {
+			it('main tool click events', function () {
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				let label = list[0].querySelector('label');
+				eventFire(label,'click', dom);
+				assert.equal(list[0].querySelector('input[type=checkbox]').checked,true);
+				assert.equal(list[1].querySelector('input[type=checkbox]').checked,false);
+				assert.equal(dom.window.changedData.apps, true);
+				assert.equal(dom.window.changedData.layers, false);
 
-	// describe('addContext(String/Element)', function () {
-	// 	let contents;
-	// 	before(function () {
-	// 		contents = dom.window.document.querySelector('#menu1 .side_content').children;
-	// 	});
+				label = list[1].querySelector('label');
+				eventFire(label,'click', dom);
+				assert.equal(list[0].querySelector('input[type=checkbox]').checked,false);
+				assert.equal(list[1].querySelector('input[type=checkbox]').checked,true);
+				assert.equal(dom.window.changedData.apps, false);
+				assert.equal(dom.window.changedData.layers, true);
+			});
 
-	// 	it(`the number of content's children matching`, function () {
-	// 		assert.equal(contents.length, 4);
-	// 	});
+			it('btn tool click events', function () {
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				let i = list[2].querySelector('i');
+				eventFire(i,'click', dom);
+				assert.equal(dom.window.changedData.value, 'btn');
+			});
+			it('check tool click events', function () {
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				let label = list[3].querySelector('label');
+				eventFire(label,'click', dom);
+				assert.equal(list[3].querySelector('input[type=checkbox]').checked,true);
+				assert.equal(dom.window.changedData.checked, true);
 
-	// 	it(`children's textContent are correct`, function () {
-	// 		[...contents].forEach(function (element, index) {
-	// 			//console.log(element);
-	// 			assert.equal(element.textContent, index + 1);
-	// 		});
+				eventFire(label,'click', dom);
+				assert.equal(list[3].querySelector('input[type=checkbox]').checked,false);
+				assert.equal(dom.window.changedData.checked, false);
+			});
+			it('multistates tool click events', function () {
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				let label = list[4].querySelector('label');
+				eventFire(label,'click', dom);
+				assert.equal(dom.window.changedData.state, 1);
 
-	// 	});
+				eventFire(label,'click', dom);
+				assert.equal(dom.window.changedData.state, 2);
 
-	// });
+				eventFire(label,'click', dom);
+				assert.equal(dom.window.changedData.state, 0);
+			});
 
-	// describe('clearContext()', function () {
-	// 	// clear context
-	// 	it('all contents was clear', function () {
-	// 		const contents = dom.window.document.querySelector('#menu2 .side_content')
-	// 		assert.equal(contents.children.length, 0);
-	// 		assert.equal(contents.textContent, '');
-	// 	});
+			it('dropdown tool click events', function () {
+				const list = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])');
+				let label = list[5].querySelector('label');
+				const li = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])')[5].querySelectorAll('ul li');
+				const checkList = dom.window.tools.elt.querySelectorAll(' .tools > li:not([class])')[5].querySelectorAll('ul li input[type=radio]');
+				
+				assert.equal(checkList[0].checked,true);
+				assert.equal(checkList[1].checked,false);
+				assert.equal(checkList[2].checked,false);
 
-	// });
+				eventFire(label,'click', dom);
+				console.log(dom.window.changedData);
+				assert.equal(dom.window.changedData.value, 'dropdown');
+				assert.equal(dom.window.changedData.checked, true);
+				assert.equal(dom.window.changedData.status, 'a');
+			});
 
-	// describe('callback function', function () {
-	// 	it(`open menu1`, function () {
-	// 		dom.window.menu1.open();
-	// 		const menu = dom.window.document.getElementById('menu1');
-	// 		const status = dom.window.document.getElementById('status');
-	// 		const name = dom.window.document.getElementById('name');
-
-	// 		assert.equal(menu.style.width, '300px');
-	// 		assert.equal(status.checked, true);
-	// 		assert.equal(name.textContent, 'menu1');
-	// 	});
-
-	// 	it(`close menu2`, function () {
-	// 		dom.window.menu2.close();
-	// 		const menu = dom.window.document.getElementById('menu2');
-	// 		const status = dom.window.document.getElementById('status');
-	// 		const name = dom.window.document.getElementById('name');
-
-	// 		assert.equal(menu.style.width, '0px');
-	// 		assert.equal(status.checked, false);
-	// 		assert.equal(name.textContent, 'menu2');
-	// 	});
-
-	// 	it(`simulate a click event on the close button`, function () {
-	// 		const closeBtn = dom.window.document.querySelector('#menu1 .close');
-	// 		eventFire(closeBtn, 'click');
-
-	// 		const menu = dom.window.document.getElementById('menu1');
-	// 		const status = dom.window.document.getElementById('status');
-	// 		const name = dom.window.document.getElementById('name');
-
-	// 		assert.equal(menu.style.width, '0px');
-	// 		assert.equal(status.checked, false);
-	// 		assert.equal(name.textContent, 'menu1');
-	// 	});
-	// });
-
-	function eventFire(el, etype) {
-		if (el.fireEvent) {
-			el.fireEvent('on' + etype);
-		} else {
-			var evObj = dom.window.document.createEvent('Events');
-			evObj.initEvent(etype, true, false);
-			el.dispatchEvent(evObj);
-		}
-	}
+	});
 
 });
