1291dcca155551c5971fe70f5fc15bf29ea83dbe
nanli <nan.li@emory.edu>
2019-02-08 17:29:50 -0500

    add export json function for labeling.

--- apps/labeling/labeling.js
+++ apps/labeling/labeling.js
@@ -39,7 +39,11 @@ function initCore(){
   }
 
   try{
-    $CAMIC = new CaMic("main_viewer",{id:$D.params.slideId}, opt);
+    let slideQuery = {}
+    slideQuery.id = $D.params.slideId
+    slideQuery.name = $D.params.slide
+    slideQuery.location = $D.params.location
+    $CAMIC = new CaMic("main_viewer",slideQuery, opt);
   }catch(error){
     Loading.close();
     $UI.message.addError('Core Initialization Failed');
@@ -48,14 +52,24 @@ function initCore(){
   }
 
   $CAMIC.loadImg(function(e){
-    //  image loaded
     if(e.hasError){
       $UI.message.addError(e.message)
+      // can't reach Slide and return to home page
+      if(e.isServiceError) redirect($D.pages.table,e.message, 0);
+    }else{
+      $D.params.data = e;
+
+      $UI.slideInfos = new CaMessage({
+      /* opts that need to think of*/
+        id:'cames',
+        defaultText:`Slide: ${$D.params.data.name}`
+      });
     }
   });
 
   $CAMIC.viewer.addHandler('open',function(){
     if($CAMIC.viewer.pmanager)$CAMIC.viewer.pmanager.on();
+    if(!$CAMIC.viewer.measureInstance) $UI.toolbar._sub_tools[2].style.display = 'none';
   });
   
   // ui init
@@ -94,31 +108,36 @@ function initCore(){
         type:'radio',
         value:'measure',
         callback:toggleMode
+      },
+      {
+        icon:'get_app',// material icons' name
+        title:'Download Labeling',
+        type:'btn',// btn/check/dropdown
+        value:'download',
+        callback:downloadLabel
+      },
+      // bug report
+      {
+        icon: 'bug_report',
+        title: 'Bug Report',
+        value: 'bugs',
+        type: 'btn',
+        callback: ()=>{window.open('https://goo.gl/forms/mgyhx4ADH0UuEQJ53','_blank').focus()}
       }
-      // ,
-      // {
-      //   icon:'share',
-      //   title:'Share View',
-      //   type:'btn',
-      //   value:'share',
-      //   callback:shareURL
-      // },
-      // {
-      //   icon:'view_carousel',
-      //   title:'Side By Side Viewer',
-      //   value:'dbviewers',
-      //   type:'check',
-      //   callback:toggleViewerMode
-      // }
-
     ]
   });
-
-
-
+}
+function downloadLabel(){
+  let text =`{"slideId":${$D.params.data['_id']['$oid']},"name":${$D.params.data['name']},"patches":${JSON.stringify($CAMIC.viewer.pmanager.exportPatchesAsJSON())}}`;
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', `${$D.params.data['name']}-Patches-${new Date().toISOString()}.json`);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 
 }
-
 function toggleMode(data){
   const mode = data.value;
   switch (mode) {
@@ -132,14 +151,11 @@ function toggleMode(data){
       $CAMIC.viewer.measureInstance.off();
       $CAMIC.viewer.pmanager.isPoint = false;
       $CAMIC.viewer.pmanager.on();
-
-      console.log('rect');
       break;
     default:
       $CAMIC.viewer.pmanager.off();
       $CAMIC.viewer.measureInstance.on();
       // statements_def
-      console.log('de');
       break;
   }
 }
