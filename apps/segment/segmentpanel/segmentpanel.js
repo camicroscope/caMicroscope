105d421c72cb10487e1da571edccb229865cebf3
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-03-11 17:13:45 -0400

    Css issues

--- apps/segment/segmentpanel/segmentpanel.js
+++ apps/segment/segmentpanel/segmentpanel.js
@@ -3,8 +3,14 @@
 
 function SegmentPanel(viewer){
 	const temp = `
-		<div class = 'material-icons settings'>close</div>
-		<div class='segment-setting'><input id="segment-slider" type='range' min=0 max=1 step=0.01 value=0.7><label>0.7</label></div>
+		<div id='close' class = 'material-icons settings'>close</div>
+		<div id='save' class = 'material-icons settings'>save</div>
+		<div id='thresh' class = 'material-icons settings'>all_out</div>
+		<div id='amin' class = 'material-icons settings'>arrow_downward</div>
+		<div id='amax' class = 'material-icons settings'>arrow_upward</div>
+		<div id='twrap' class='segment-setting' hidden='true'><input id='threshold' class='segment-slider' type='range' min=0 max=1 step=0.01 value=0.7><label id='tlabel'>0.7</label></div>
+		<div id='minwrap' class='segment-setting' hidden='true'><input id='minarea' class='segment-slider' type='range' min=0 max=1 step=1 value=400><label>400</label></div>
+		<div id='maxwrap' class='segment-setting' hidden='true'><input id='maxarea' class='segment-slider' type='range' min=0 max=5000 step=1 value=4500><label>4500</label></div>
 		<div class='segment-count'><label>Object Count: </label><label id='segcount'></label></div>
 		<canvas class='out'></canvas>
 		<canvas class='src'></canvas>
@@ -15,17 +21,31 @@ function SegmentPanel(viewer){
 	this.elt.innerHTML = temp;
 	this.__out = this.elt.querySelector('.out');
 	this.__src = this.elt.querySelector('.src');
-	this.__input = this.elt.querySelector('.segment-setting input[type=range]');
-	this.__label = this.elt.querySelector('.segment-setting label');
+	this.__input = this.elt.querySelector('.segment-setting input[type=range]#threshold');
+	this.__label = this.elt.querySelector('.segment-setting label#tlabel');
+	this.__twrap = this.elt.querySelector('#twrap');
 	this.viewer.addOverlay({
       element: this.elt,
       location: new OpenSeadragon.Rect(0,0,0,0),
       checkResize: false
     });
     this.overlay = this.viewer.currentOverlays[this.viewer.currentOverlays.length-1];
-    this.elt.querySelector('.material-icons').addEventListener('click',function(e){
+    this.elt.querySelector('#close').addEventListener('click',function(e){
     	this.close();
-    }.bind(this));
+		}.bind(this));
+		this.elt.querySelector('#save').addEventListener('click',function(e){
+    	this.save();
+		}.bind(this));
+		this.elt.querySelector('#thresh').addEventListener('click',function(e){
+    	this.showThreshBar();
+		}.bind(this));
+		this.elt.querySelector('#amin').addEventListener('click',function(e){
+    	this.save();
+		}.bind(this));
+		this.elt.querySelector('#amax').addEventListener('click',function(e){
+    	this.save();
+		}.bind(this));
+		
     this.close()
 }
 
@@ -36,6 +56,21 @@ SegmentPanel.prototype.open = function(){
 SegmentPanel.prototype.close = function(){
 	this.elt.style.display = 'none';
 };
+
+SegmentPanel.prototype.save = function(){
+	alert('Saving Image and Mask!');
+};
+
+SegmentPanel.prototype.showThreshBar = function(){
+	if(this.__twrap.getAttribute('hidden') == 'true') {
+		this.__twrap.setAttribute('hidden', 'false');
+	} else {
+		this.__twrap.setAttribute('hidden', 'true');
+	}
+	// alert('Showing Bar');
+};
+
+
 SegmentPanel.prototype.setPosition = function(x,y,w,h){
 	this.overlay.location.x = x;
 	this.overlay.location.y = y;
