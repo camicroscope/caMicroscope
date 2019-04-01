77231db00f8f6648f51d38cb43cbc484158dab1f
Ryan Birmingham <birm@rbirm.us>
2019-04-01 12:58:35 -0400

    try to fix map locations to supress warns

--- dist/packages.js
+++ dist/packages.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRe
 },{}],"qMUh":[function(require,module,exports) {
 "use strict";var e=require("./test_package.js"),u=l(e),r=require("./DummyStore.js"),t=l(r),s=require("./ParamPoly.js"),d=l(s);function l(e){return e&&e.__esModule?e:{default:e}}a=new u.default("Buildy"),(0,t.default)(),(0,d.default)("passed");
 },{"./test_package.js":"JNGe","./DummyStore.js":"TxTK","./ParamPoly.js":"xeEA"}]},{},["qMUh"], null)
-//# sourceMappingURL=/packages.map
+//# sourceMappingURL=./packages.map
