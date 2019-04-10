b3bd5f3c1c1f3464b2dc5e3ec775deb0ae862659
nanli <nan.li@emory.edu>
2019-04-10 12:25:31 -0400

    fix statehelper error

--- package/pathdb_package.js
+++ package/pathdb_package.js
@@ -242,14 +242,16 @@ function PathDbMods() {
     })
   };
 
-  StatesHelper.prototype.default_getCurrentStatesURL = StatesHelper.prototype.getCurrentStatesURL;
-  getCurrentStatesURL = function(isImageCoordinate=false){
-    let states = StatesHelper.getCurrentStates(isImageCoordinate);
-    if(!states)return;
-    console.log(states);
-    states = StatesHelper.encodeStates(states);
-    return `${location.origin}${location.pathname}?slideId=${$D.params.slideId}&states=${$D.params.states}&mode=${$D.params.mode}`
-  };
+  if(StatesHelper){
+    StatesHelper.prototype.default_getCurrentStatesURL = StatesHelper.prototype.getCurrentStatesURL;
+    getCurrentStatesURL = function(isImageCoordinate=false){
+      let states = StatesHelper.getCurrentStates(isImageCoordinate);
+      if(!states)return;
+      console.log(states);
+      states = StatesHelper.encodeStates(states);
+      return `${location.origin}${location.pathname}?slideId=${$D.params.slideId}&states=${$D.params.states}&mode=${$D.params.mode}`
+    };
+  }
 
 
   CaMic.prototype.default_loadImg = CaMic.prototype.loadImg
