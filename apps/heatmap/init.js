0415c2e8c09d3d892a73d3cffbfb6a43630c2bb9
Ryan Birmingham <birm@rbirm.us>
2019-06-10 16:09:20 -0400

    forgot study in find in init

--- apps/heatmap/init.js
+++ apps/heatmap/init.js
@@ -72,11 +72,12 @@ function initCore(){
       $D.heatMapData = await $CAMIC.store.getHeatmap($D.params.data.name,$D.params.execId, $D.params.data.study, $D.params.data.specimen).then(d=> d[0]);
       const specimen = $D.heatMapData.provenance.image.specimen;
       const slide = $D.heatMapData.provenance.image.slide;
+      const study = $D.heatMapData.provenance.image.study;
       const exec = $D.heatMapData.provenance.analysis.execution_id;
 
       if(ImgloaderMode!='imgbox'){
         // query from DB
-        const editData = await $CAMIC.store.findHeatmapEdit(getUserId(), specimen, slide, exec).then(d=>d[0]);
+        const editData = await $CAMIC.store.findHeatmapEdit(getUserId(), specimen, slide, study, exec).then(d=>d[0]);
         //const editData = await $CAMIC.store.findHeatmapEdit('test', specimen, slide, exec).then(d=>d[0]);
         $D.editedDataClusters = new EditDataCluster();
         if(editData&&Array.isArray(editData.data)&&editData.data.length > 0){
