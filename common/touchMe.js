80f920baa4559e788b0c68ace53ea75d815992fc
Ryan Birmingham <birm@rbirm.us>
2019-07-15 17:28:57 -0400

    Update touchMe.js

--- common/touchMe.js
+++ common/touchMe.js
@@ -1,12 +1,12 @@
 function touchHandler(event)
 {
     console.info(event)
-    var touches = event.changedTouches;
+    var touches = event.touches;
     console.log(touches)
     if (!touches.length){
      return
     }
-    var first = touches[touches.length-1]; // or last?
+    var first = touches[0]; // or last?
     var type = "";
     switch(event.type)
     {
@@ -19,7 +19,9 @@ function touchHandler(event)
     // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
     //                screenX, screenY, clientX, clientY, ctrlKey, 
     //                altKey, shiftKey, metaKey, button, relatedTarget);
-
+    if (type=="mousemove"){
+        event.preventDefault();
+    }
     var simulatedEvent = document.createEvent("MouseEvent");
     simulatedEvent.initMouseEvent(type, true, true, window, 1, 
                                   first.screenX, first.screenY, 
