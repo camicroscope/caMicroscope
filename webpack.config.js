c4b44edc8fcdf48d3574dc9189176d6b19945589
sumit <sumit.pradhan65@gmail.com>
2020-03-15 10:09:21 +0530

    corrected location and filename

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 module.exports = {
     mode: 'development',
-    entry: './test/extension/webpack-draw-polygon.js',
+    entry: './test/extension/webpack-draw-polygon-test.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle-draw-polygon.js'
+        filename: 'bundle-draw-polygon-test.js'
     },
     module: {
         rules: [{
