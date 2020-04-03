b0509cf38598e12f95fa71f86dbb5c88a0b3da13
Chirag Jain <cjchirag7@gmail.com>
2020-04-03 21:58:07 +0530

    Remove camel case ignore rule in ESLint (#278)
    * Made all identifers in components/ in camelCase to remove camelCase rule of ESLint
    
    * Made identifiers in apps folder to be in camelCase
    
    * changed anno_delte to annoDelte
    
    * Fixed camelCase in init.js and table.html
    
    * Update spyglass.js
    
    merge request #278
    variable name changing ====> `_target_viewer` => `_targetViewer`.
    
    * Update uicallbacks.js
    
    1. `toolbar._main_tools` => `toolbar._mainTools`, there are 8 places in `apps/veiwer/uicallbacks.js` that need to change when you change the variable`_main_tools` to `_mainTools`  in `components/toolbar/toolbar.js`. [line 17, 23, 1053, 1079, 1092, 1096, 1215, 1221]
    2. `toolbar._sub_tools` => `toolbar._subTools`, there are 3 places in `apps/segment/segment.js`(2 places - line 850, 851 removed), `apps/labeling/labeling.js` (1 place - line 84) that need to change when you change the variable`_sub_tools` to `_subTools`  in `components/toolbar/toolbar.js`.
    
    * Update labeling.js
    
    `toolbar._sub_tools` => `toolbar._subTools`
    
    * Update segment.js
    
    `toolbar._sub_tools` => `toolbar._subTools`
    
    Co-authored-by: nanli-emory <40213692+nanli-emory@users.noreply.github.com>

--- apps/loader/loader.js
+++ apps/loader/loader.js
@@ -1,6 +1,6 @@
-var upload_url = '../loader/upload/start/';
-var check_url = '../loader/data/one/';
-var thumb_url = '../loader/data/thumbnail/';
+var uploadUrl = '../loader/upload/start/';
+var checkUrl = '../loader/data/one/';
+var thumbUrl = '../loader/data/thumbnail/';
 
 var store = new Store('../data/');
 
@@ -54,7 +54,7 @@ function changeStatus(step, text, reset=true) {
       if (step == 'CHECK') {
         // During check, thumbnail needs to be fetched & added to the table
         // In this case, text[col[col.length - 1]] is the filename
-        fetch(thumb_url + text[col[col.length - 1]], {credentials: 'same-origin'}).then(
+        fetch(thumbUrl + text[col[col.length - 1]], {credentials: 'same-origin'}).then(
             (response) => response.json(), // if the response is a JSON object
         ).then((x)=>{
           var tabCell = tr.cells[tr.cells.length-1];
@@ -84,7 +84,7 @@ function handleUpload(file, filename) {
   data.append('file', file);
   data.append('filename', filename);
   changeStatus('UPLOAD', 'Begun upload');
-  fetch(upload_url, {
+  fetch(uploadUrl, {
     credentials: 'same-origin',
     method: 'POST',
     body: data,
@@ -98,7 +98,7 @@ function handleUpload(file, filename) {
 }
 
 function handleCheck(filename, reset, id) {
-  fetch(check_url + filename, {credentials: 'same-origin'}).then(
+  fetch(checkUrl + filename, {credentials: 'same-origin'}).then(
       (response) => response.json(), // if the response is a JSON object
   ).then(
       (success) => {
@@ -113,7 +113,7 @@ function handleCheck(filename, reset, id) {
 }
 
 function handlePost(filename, slidename, filter, reset) {
-  fetch(check_url + filename, {credentials: 'same-origin'}).then(
+  fetch(checkUrl + filename, {credentials: 'same-origin'}).then(
       (response) => response.json(), // if the response is a JSON object
   ).then(
       (data) => {
