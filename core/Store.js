a351023a6dfc5a6cc0960312b2c518d9744018b9
Hemansh31 <hemansh3172@gmail.com>
2020-04-06 01:33:40 +0530

    Initial Implementation of Information Dashboard

--- core/Store.js
+++ core/Store.js
@@ -171,6 +171,16 @@ class Store {
       mode: 'cors',
     }).then(this.errorHandler).then((x)=>this.filterBroken(x, 'mark'));
   }
+  fetchMark(slide_id) {
+    const suffix = 'Mark/find';
+    const url = this.base + suffix;
+    const query = {};
+    query['provenance.image.slide'] = slide_id;
+    return fetch(url + '?' + objToParamStr(query), {
+      credentials: 'include',
+      mode: 'cors',
+    }).then(this.errorHandler).then((x)=>this.filterBroken(x, 'mark'));
+  }
   /**
    * post mark
    * @param {object} json - the mark data
@@ -249,6 +259,16 @@ class Store {
     if (slide) {
       query['provenance.image.slide'] = slide;
     }
+    return fetch(url + '?' + objToParamStr(query), {
+      credentials: 'include',
+      mode: 'cors',
+    }).then(this.errorHandler).then((x)=>this.filterBroken(x, 'heatmap'));
+  }
+  fetchHeatMap(slide_id) {
+    const suffix = 'Heatmap/find';
+    const url = this.base + suffix;
+    const query = {};
+    query['provenance.image.slide'] = slide_id;
     return fetch(url + '?' + objToParamStr(query), {
       credentials: 'include',
       mode: 'cors',
