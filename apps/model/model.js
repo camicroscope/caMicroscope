7b94e042ac151630f59c6b7cc34eb3aa7ba058e0
Amritansh Vajpayee <amritansh.222000@gmail.com>
2020-04-10 12:44:21 +0530

    Update model.js

--- apps/model/model.js
+++ apps/model/model.js
@@ -807,10 +807,10 @@ async function showInfo() {
             td.innerHTML = date;
             td = row.insertCell();
             td.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" ' +
-            'id="removeModel" type="button">Remove Model</button>';
+            'id="removeModel" type="button"><i class="material-icons" style="font-size:16px;">delete_forever</i>Remove Model</button>';
             td = row.insertCell();
             td.innerHTML = '<button class="btn btn-primary btn-xs my-xs-btn" '+
-            'id="chngClassListBtn" type="button">Edit Class List</button>';
+            'id="chngClassListBtn" type="button"><i class="material-icons" style="font-size:16px;">edit</i>  Edit Classes</button>';
             document.getElementById('removeModel').addEventListener('click', () => {
               deleteModel(name);
             });
