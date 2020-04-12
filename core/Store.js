fcf2bfec78a8e1847745ff5bb5fc0ad5a79ce320
Vedant  Nandoskar <vedant.nandoskar@gmail.com>
2020-04-12 17:02:01 +0530

    style fix

--- core/Store.js
+++ core/Store.js
@@ -700,7 +700,7 @@ class Store {
   * @param {object} reqId - the request object id
   * @return {promise} - promise which resolves with response
   **/
-  cancelRequestToDeleteSlide(reqId, onlyRequestCancel=true) { 
+  cancelRequestToDeleteSlide(reqId, onlyRequestCancel=true) {
     // If only cancelling request and not deleting slide file then set onlyRequestCancel to true
     const suffix = 'Request/delete';
     const url = this.base + suffix;
