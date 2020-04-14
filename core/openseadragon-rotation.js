edf3080629c35f2dad46eaf8086481fefb17857e
Hemansh31 <hemansh3172@gmail.com>
2020-04-14 17:13:18 +0530

    Resolved lint fixes

--- core/openseadragon-rotation.js
+++ core/openseadragon-rotation.js
@@ -1,16 +1,16 @@
-(function($){
+(function($) {
   if (!$.version || $.version.major < 2) {
     throw new Error('This version of OpenSeadragonScalebar requires ' +
                 'OpenSeadragon version 2.0.0+');
   }
-  $.Viewer.prototype.rotationBar = function(options){
+  $.Viewer.prototype.rotationBar = function(options) {
     options = options || {};
     options.viewer = this;
     this.rotateBar = new $.RotationBar(options);
-  }
-  $.RotationBar = function(options){
+  };
+  $.RotationBar = function(options) {
     options = options || {};
-    if(!options.viewer){
+    if (!options.viewer) {
       throw new Error('A viewer must be specified.');
     }
     this.viewer = options.viewer;
@@ -40,7 +40,7 @@
     this.idxR.classList.add('idxR');
     this.txtR = document.createElement('div');
     this.txtR.classList.add('txtR');
-    this.txtR.innerHTML = '0'
+    this.txtR.innerHTML = '0';
     this.degree = document.createElement('sup');
     this.degree.innerHTML = 'o';
     this.degree.style.marginLeft = '2px';
@@ -52,11 +52,11 @@
     const idk=this.txtR;
     const sup = this.degree;
     const view = this.viewer;
-    this.range.oninput = function(){
+    this.range.oninput = function() {
       idk.innerHTML = this.value;
       idk.appendChild(sup);
       var intValue = parseInt(this.value);
       view.viewport.setRotation(intValue);
-    }
-  }
-}(OpenSeadragon))
+    };
+  };
+}(OpenSeadragon));
