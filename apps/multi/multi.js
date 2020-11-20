fb7043b07e6e403c72b9b06a4826c407545649e7
Ryan Birmingham <birm@rbirm.us>
2020-11-20 09:10:26 -0500

    multiapp planning

---
+++ apps/multi/multi.js
@@ -0,0 +1,15 @@
+// initalize store, etc
+
+// run the query, if query, to get list
+// use generated (or alt passed) list to open slides
+// for each in list
+  // use store to get slide info
+  // TODO pathdb variant
+  // create an appropriately sized div for all
+  // create OSD env
+  // create "open this slide button"
+  // style
+  // append
+
+
+// ?? how sustainable will this be?
