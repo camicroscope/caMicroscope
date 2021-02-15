9245a71745fe85f81de6ed93e8189f42105db23e
r7rohan <rohanmallick711@gmail.com>
2021-02-15 18:39:41 +0530

    toolbar and tour fixes in develop

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -155,6 +155,7 @@ CaToolbar.prototype.__createBtn = function(options) {
   btn.classList.add('md-24');
   btn.textContent = options.icon;
   if (options.title) {
+    btn.title = options.title;
     tippy(btn, {
       content: options.title,
       placement:'right',
@@ -213,12 +214,13 @@ CaToolbar.prototype.__createCheck = function(options) {
   icon.textContent = options.icon;
   icon.htmlFor = id;
   if (options.title) {
+    icon.title = options.title;
     tippy(icon, {
       content: options.title,
       placement:'right',
       delay: 300,
       theme:'light-border'
-    });    
+    });
   }
   li.appendChild(icon);
 
@@ -289,12 +291,13 @@ CaToolbar.prototype.__createRadio = function(options) {
 
   icon.htmlFor = id;
   if (options.title) {
+    icon.title = options.title;
     tippy(icon, {
       content: options.title,
       placement:'right',
       delay: 300,
       theme:'light-border'
-    });    
+    });
   }
 
   li.appendChild(icon);
@@ -334,12 +337,13 @@ CaToolbar.prototype.__createMultiStateBtns = function(options) {
   icon.dataset.state = 0;
   icon.textContent = options.icon;
   if (options.title) {
+    icon.title = options.title;
     tippy(icon, {
       content: options.title,
       placement:'right',
       delay: 300,
       theme:'light-border'
-    });    
+    });
   }
   li.appendChild(icon);
   li.addEventListener('click', function(e) {
@@ -386,12 +390,13 @@ CaToolbar.prototype.__createMultiDropDown = function(options) {
   icon.textContent = options.icon;
   icon.htmlFor = id;
   if (options.title) {
+    icon.title = options.title;
     tippy(icon, {
       content: options.title,
       placement:'right',
       delay: 300,
       theme:'light-border'
-    });    
+    });
   }
   li.appendChild(icon);
 
@@ -513,6 +518,7 @@ CaToolbar.prototype.__createDropDown = function(options) {
   icon.textContent = options.icon;
   icon.htmlFor = id;
   if (options.title) {
+    icon.title = options.title;
     tippy(icon, {
       content: options.title,
       placement:'right',
