90920f775361499bfcde8feb34c4d2ca8d07f77c
Ryan Birmingham <birm@rbirm.us>
2021-05-18 18:06:55 -0400

    structure for vistable

--- apps/vis-table/vis-table.js
+++ apps/vis-table/vis-table.js
@@ -1,9 +1,37 @@
 // render each thumbnail
-function renderSlide(data){
-  return 0
+function renderSlide(data) {
+  // make div
+  let div = document.createElement('div');
+  div.classList.add('slidebox');
+  let location = data.location;
+  // add thumbnail
+  let thumbnailUrl = `../../img/IIP/raw/?FIF=${location}&WID=200&CVT=png`;
+  div.style.backgroundImage = `url(${thumbnailUrl});`;
+  // add text
+  div.innerText = data.name;
+  // add checkmark if reviewed, for now
+  if (data.review) {
+    div.innerText += '✔';
+  }
+  // add to a link
+  let anchor = document.createElement('a');
+  anchor.href = `../viewer/viewer.html?slideId=${data['_id']['$oid']}`;
+  anchor.appendChild(div);
+  document.getElementById('table').appendChild(anchor);
 }
 
-// initialize the table
-init(){
-
+// initialization routine
+function init(filters) {
+  const STORE = Store('../../data');
+  // get slide data w/filters
+  STORE.findSlide(filters).then((x)=>{
+    x.forEach(renderSlide);
+  });
+  // render each one
 }
+
+
+// get url params
+filters = {};
+// initialize with url params as filters
+init(filters);
