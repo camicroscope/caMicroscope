178b6c761bf30eaf83dad37553f8fb8faf8c109d
Ryan Birmingham <birm@rbirm.us>
2022-09-09 21:07:01 -0400

    lintfix export

--- apps/port/tree_table.js
+++ apps/port/tree_table.js
@@ -1,64 +1,64 @@
-function make_tree_table(id){
+function makeTreeTable(id) {
+  var
+    $table = $('#' + id);
+  var rows = $table.find('tr');
+
+  rows.each(function(index, row) {
     var
-        $table = $('#' + id),
-        rows = $table.find('tr');
+      $row = $(row);
+    var level = $row.data('level');
+    var id = $row.data('id');
+    var $columnName = $row.find('td[data-column="name"]');
+    var children = $table.find('tr[data-parent="' + id + '"]');
 
-    rows.each(function (index, row) {
-        var
-            $row = $(row),
-            level = $row.data('level'),
-            id = $row.data('id'),
-            $columnName = $row.find('td[data-column="name"]'),
-            children = $table.find('tr[data-parent="' + id + '"]');
-
-        if (children.length) {
-            var expander = $columnName.prepend('' +
+    if (children.length) {
+      var expander = $columnName.prepend('' +
                 '<span class="treegrid-expander glyphicon glyphicon-chevron-right">&#9660;</span>' +
                 '');
 
-            children.hide();
+      children.hide();
 
-            expander.on('click', function (e) {
-                var $target = $(e.target);
-                if ($target.hasClass('glyphicon-chevron-right')) {
-                    $target
-                        .removeClass('glyphicon-chevron-right')
-                        .addClass('glyphicon-chevron-down');
+      expander.on('click', function(e) {
+        var $target = $(e.target);
+        if ($target.hasClass('glyphicon-chevron-right')) {
+          $target
+              .removeClass('glyphicon-chevron-right')
+              .addClass('glyphicon-chevron-down');
 
-                    children.show();
-                } else {
-                    $target
-                        .removeClass('glyphicon-chevron-down')
-                        .addClass('glyphicon-chevron-right');
+          children.show();
+        } else {
+          $target
+              .removeClass('glyphicon-chevron-down')
+              .addClass('glyphicon-chevron-right');
 
-                    reverseHide($table, $row);
-                }
-            });
+          reverseHide($table, $row);
         }
+      });
+    }
 
-        $columnName.prepend('' +
+    $columnName.prepend('' +
             '<span class="treegrid-indent" style="width:' + 25 * level + 'px"></span>' +
             '');
-    });
+  });
 
-    // Reverse hide all elements
-    reverseHide = function (table, element) {
-        var
-            $element = $(element),
-            id = $element.data('id'),
-            children = table.find('tr[data-parent="' + id + '"]');
+  // Reverse hide all elements
+  reverseHide = function(table, element) {
+    var
+      $element = $(element);
+    var id = $element.data('id');
+    var children = table.find('tr[data-parent="' + id + '"]');
 
-        if (children.length) {
-            children.each(function (i, e) {
-                reverseHide(table, e);
-            });
+    if (children.length) {
+      children.each(function(i, e) {
+        reverseHide(table, e);
+      });
 
-            $element
-                .find('.glyphicon-chevron-down')
-                .removeClass('glyphicon-chevron-down')
-                .addClass('glyphicon-chevron-right');
+      $element
+          .find('.glyphicon-chevron-down')
+          .removeClass('glyphicon-chevron-down')
+          .addClass('glyphicon-chevron-right');
 
-            children.hide();
-        }
-    };
+      children.hide();
+    }
+  };
 }
