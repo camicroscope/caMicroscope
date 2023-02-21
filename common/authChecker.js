9cacf57479462926bc06912d104a8a930f61b647
Ryan Birmingham <birm@rbirm.us>
2023-02-21 00:19:03 -0500

    no user find store

--- common/authChecker.js
+++ common/authChecker.js
@@ -10,40 +10,29 @@ const __auth_check = async function(levels) {
   }
 };
 
-const checkRegistrationStatus = async function(levels) {
+const checkRegistrationStatus = function(levels) {
   const base = `${'../'.repeat(levels)}data/`;
-  const dbStore = new Store(base);
   try {
-    var user = await dbStore.getUsers(getUserId());
-    if (Array.isArray(user)&&user.length>0) { // user exist
-      $USER = user = user[0];
-      if (user.registration&&user.isAgreed) { //
-        const userType = getUserType();
-        switch (userType) {
-          case 'Admin':
-            if (-1 === window.location.href.indexOf('landing/landing.html')) {
-              window.location = '../'.repeat(levels) + 'apps/landing/landing.html';
-            }
-            break;
-          case 'Editor':
-            if (-1 === window.location.href.indexOf('landing/crowd.html')) {
-              window.location = '../'.repeat(levels) + 'apps/landing/crowd.html';
-            }
-            break;
-          case 'Null':
-            if (-1 === window.location.href.indexOf('landing/crowd.html')) {
-              window.location = '../'.repeat(levels) + 'apps/landing/crowd.html';
-            }
-            break;
-          default:
-            console.warn('something wrong');
-            break;
+    const userType = getUserType();
+    switch (userType) {
+      case 'Admin':
+        if (-1 === window.location.href.indexOf('landing/landing.html')) {
+          window.location = '../'.repeat(levels) + 'apps/landing/landing.html';
         }
-      } else { // no registration info and disagree consent form
-        window.location = '../'.repeat(levels) + 'apps/registration/registration.html';
-      }
-    } else {
-      window.location = '../'.repeat(levels) + 'login.html?state=' + encodeURIComponent(window.location);
+        break;
+      case 'Editor':
+        if (-1 === window.location.href.indexOf('landing/crowd.html')) {
+          window.location = '../'.repeat(levels) + 'apps/landing/crowd.html';
+        }
+        break;
+      case 'Null':
+        if (-1 === window.location.href.indexOf('landing/crowd.html')) {
+          window.location = '../'.repeat(levels) + 'apps/landing/crowd.html';
+        }
+        break;
+      default:
+        console.warn('something wrong');
+        break;
     }
   } catch (error) {
     console.error('checkRegistrationStatus: service error'+ error.toString());
