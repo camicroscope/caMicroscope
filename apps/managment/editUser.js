9a282f5c3dedac48c2ea79ed583d04ee291c7da5
Ryan Birmingham <birm@rbirm.us>
2023-02-28 01:55:15 -0500

    partially tested managment utils

---
+++ apps/managment/editUser.js
@@ -0,0 +1,94 @@
+const store = new Store('../../data/');
+
+async function populateUserEdit(){
+  let collections = await store.getAllCollection()
+  let searchparam = new URLSearchParams(window.location.search)
+  let users = await store.getUsers(searchparam.email[0]);
+  let user = users[0];
+
+  // populate regular fields
+  document.getElementById('first_name').value = user.registration.firstName;
+  document.getElementById('last_name').value = user.registration.lastName;
+  document.getElementById('email').value = user.email;
+  document.getElementById('institution').value = user.registration.institutionOfEmployment;
+  // hidden user id
+  document.getElementById('user_id').value = user['_id']['$oid'];
+  // userType
+  if (user.userType == "Admin"){
+    document.getElementById('admin-select').checked=true;
+  }
+  else if (user.userType == "Editor"){
+    document.getElementById('participant-select').checked=true;
+  }
+  else if (user.userType == "Expert"){
+    document.getElementById('expert-select').checked=true;
+  } else {
+    document.getElementById('public-select').checked=true;
+  }
+
+  // collections
+  let collectionMap = {}
+  let collectionSelector = document.getElementById('collection_list');
+  for (let i of collections){document.getElementById('first_name').value
+    // populate collection map
+    collectionMap[i['_id']['$oid']] = i['name']
+    // add checkboxes for collection selection
+    let outer_div = document.createElement("div")
+    outer_div.classList.add("form-check", "form-switch")
+    let input=document.createElement("input")
+    input.id = "check-collection-" + i['name']
+    input.name = i['name']
+    input.value = i['_id']['$oid']
+    input.type = "checkbox"
+    input.classList.add("checkbox", "form-check-input" ,"collection_input")
+    // set checked to true if present
+    if (user.collections.indexOf(i['_id']['$oid'])>=0){
+      input.checked = true;
+    }
+    // make label
+    let label = document.createElement("label")
+    label.classList.add("form-check-label")
+    label.for = "check-collection-" + i['name']
+    label.innerText = i['name']
+    // append to document
+    outer_div.appendChild(input);
+    outer_div.appendChild(label);
+    collectionSelector.appendChild(outer_div);
+  }
+}
+
+async function updateUser(e){
+  console.log(e)
+  let user_id = document.getElementById("user_id").value
+  let edits = {};
+  edits.registration = {};
+  edits.registration.firstName = document.getElementById('first_name').value;
+  edits.registration.lastName = document.getElementById('last_name').value;
+  edits.registration.institutionOfEmployment = document.getElementById('institution').value;
+  edits.email = document.getElementById('email').value;
+  userTypeBoxes = document.getElementById("user_type").children;
+  // user type
+  if (document.getElementById('expert-select').checked){
+    edits.userType = "Expert"
+  } else if (document.getElementById('admin-select').checked){
+    edits.userType = "Admin"
+  } else if (document.getElementById('editor-select').checked){
+    edits.userType = "Editor"
+  } else {
+    edits.userType = "Null"
+  }
+  // collections
+  edits.collections = [];
+  let collectionBoxes = document.getElementsByClassName("collection_input");
+  for (let i of collectionBoxes){
+    if (i.checked){
+      edits.collections.push(i.value)
+    }
+  }
+  // update the user
+  let res = await store.updateUser(user_id, edits)
+  console.log(edits)
+  return edits;
+}
+
+document.getElementById("updateBtn").addEventListener("click", updateUser);
---
+++ apps/managment/users.js
@@ -0,0 +1,66 @@
+const store = new Store('../../data/');
+
+async function populateUserTable(){
+  // todo use await store instead
+  let collections = await store.getAllCollection()
+
+  let users = await store.getUsers();
+
+  // map between collection id and name
+  let collectionMap = {}
+  for (let i of collections){
+    collectionMap[i['_id']['$oid']] = i['name']
+  }
+
+
+  // cleaned user data for the table
+  let usersClean = []
+  for (let i of users){
+    let u = {};
+    u['Name'] = i.registration.firstName + " " + i.registration.lastName
+    u['Email'] = i.email
+    u['Institution'] = i.registration.institutionOfEmployment
+    u['Type'] = i.userType
+    u['Collections'] = ""
+    for(j of i.collections){
+      u['Collections'] += collectionMap[j] || j
+      u['Collections'] += ", "
+    }
+    // remove last comma and space
+    u['Collections'] = u['Collections'].slice(0,-2)
+    usersClean.push(u)
+  }
+
+  // populate table with usersClean result
+
+  for (user of usersClean){
+    let tr = document.createElement("tr");
+    let name_td = document.createElement("td");
+    name_td.innerText = user['Name']
+    tr.appendChild(name_td);
+    let institution_td = document.createElement("td");
+    institution_td.innerText = user['Institution']
+    tr.appendChild(institution_td);
+    let email_td = document.createElement("td");
+    email_td.innerText = user['Email']
+    tr.appendChild(email_td);
+    let type_td = document.createElement("td");
+    type_td.innerText = user['Type']
+    tr.appendChild(type_td);
+    let collections_td = document.createElement("td");
+    collections_td.innerText = user['Collections']
+    tr.appendChild(collections_td);
+    // edit button
+    let edit_td = document.createElement("td");
+    let edit_btn = document.createElement("button")
+    edit_btn.innerText = "Edit"
+    edit_btn.type = "button"
+    edit_btn.classList.add("btn", "btn-primary")
+    // TODO go to appropriate edit url
+    edit_td.appendChild(edit_btn)
+    tr.appendChild(edit_td);
+
+    document.getElementById("user_table_body").appendChild(tr);
+  }
+
+}
