5b659e992c2b25bd074cb07b626f1d8402fd3d6f
Ryan Birmingham <birm@rbirm.us>
2023-03-07 13:43:57 -0500

    duplicate getCookie for auth check dep

--- common/authChecker.js
+++ common/authChecker.js
@@ -1,3 +1,14 @@
+function getCookie(name) {
+  var value = '; ' + document.cookie;
+  var parts = value.split('; ' + name + '=');
+  if (parts.length == 2) {
+    return parts
+        .pop()
+        .split(';')
+        .shift();
+  }
+}
+
 const __auth_check = async function(levels) {
   // check token
   if (!getCookie('token')) {
