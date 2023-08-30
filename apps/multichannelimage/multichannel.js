d973cae49455539ad97282183e4a9866e88c922b
lucifertrj <jaintarun.abd17@gmail.com>
2023-08-30 22:49:21 +0530

    image async function

--- apps/multichannelimage/multichannel.js
+++ apps/multichannelimage/multichannel.js
@@ -1,13 +1,11 @@
 async function fetchData() {
   try {
     console.log("hello");
-    
     const response = await fetch("../../multichannel/", {
       method: "GET",
     });
-    
     const data = await response.json();
-    
+    console.log("Working...")
     if (data.success) {
       console.log(data);
     } else {
@@ -18,5 +16,23 @@ async function fetchData() {
   }
 }
 
-// Call the async function to fetch the data
+async function imageData() {
+  try {
+    console.log("hello image");
+    
+    const response = await fetch("../../multichannel/", {
+      method: "GET",
+    });
+    const data = await response.json();
+    console.log("Multi channel Image support")
+    if (data.success) {
+      console.log(data);
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
 fetchData();
