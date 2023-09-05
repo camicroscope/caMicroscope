5abd8ebf74dcc6977aff68ef6395a44919fe6157
Ryan Birmingham <birm@rbirm.us>
2023-09-05 15:15:08 -0400

    go to collection viewer instead of pitfalls table

--- apps/labeling/pitfalls.js
+++ apps/labeling/pitfalls.js
@@ -178,7 +178,7 @@ async function initialize() {
       // direct user away if they don't have access
       if (getUserInfo().userType == "Participant"){
         alert("Error: Your user account type, 'Participant', does not have permission to contribute to ROI selection. If you were assigned an ROI selection task and need to change your account type, please contact Brandon.Gallas@fda.hhs.gov.");
-        window.location = `./pitfallsTable.html?collectionId=${$D.params.collectionId}`;
+        window.location = "../collection/viewer.html";
       }
 
       // if (!$D.ROI) redirect($D.pages.table, 'There Is No Label Data, Return Home Page.', 0);
