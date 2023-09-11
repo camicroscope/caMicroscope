6d1dd04add396498a7780b09d52cbc68ff4802f8
lucifertrj <jaintarun.abd17@gmail.com>
2023-09-11 21:42:57 +0530

    try viewer route

---
+++ apps/multichannelimage/viewer.js
@@ -0,0 +1,28 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+const dziPath = getParameterByName("dziPath");
+const channelOrder = getParameterByName("channel_order");
+
+const viewer = OpenSeadragon({
+    id: "openseadragon-viewer",
+    prefixUrl: "../../core/openseadragon/images/",
+    tileSources: "../static/" + dziPath,
+    preserveViewport: true,
+    visibilityRatio: 1,
+    defaultZoomLevel: 0,
+    maxZoomPixelRatio: 1
+});
+function updateChannelOrder() {
+    const currentImage = viewer.world.getItemAt(0).getItems()[0];
+    currentImage.setChannelOrder(channelOrder);
+    currentImage.draw();
+}
+updateChannelOrder();
