8b248c12945889327a1496e69ebe7200996c94b4
lucifertrj <jaintarun.abd17@gmail.com>
2023-11-04 19:16:01 +0530

    channel js path mismatch

--- apps/multichannelimage/multichannel.js
+++ apps/multichannelimage/multichannel.js
@@ -1,39 +1,14 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Response/ok - to check the valid response
-
-document.getElementById('chooseButton').addEventListener('click', fetchData);
-
-    async function fetchData() {
-        try {
-            console.log("hello");
-            const response = await fetch("../../multichannel/", {
-                method: "GET",
-            });
-            if (response.ok) {
-                const data = await response.json();
-                console.log("Route, direct Working...");
-                console.log(data);
-            }
-        } catch (error) {
-            console.log("Error:", error);
-        }
-    }
-
-async function imageData() {
-  try {
-    console.log("hello image");
-    const response = await fetch("../../multichannel/44153.tif", {
-      method: "GET",
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('../../multichannel/')
+      .then(response => response.json())
+      .then(data => {
+          const imageList = document.getElementById('imageList');
+          data.sample_files.forEach(file => {
+              const imgElement = document.createElement('img');
+              imgElement.src = `static/mat_files/${file}.jpg`; // Assuming you have JPEG versions for preview
+              imgElement.alt = `Sample Image ${file}`;
+              imageList.appendChild(imgElement);
+          });
+      })
+      .catch(error => console.error('Error:', error));
     });
-    const data = await response.json();
-    console.log("Multi channel Image support")
-    if (data.ok) {
-      console.log(data);
-    } else {
-      alert(data.message);
-    }
-  } catch (error) {
-    console.log("Error:", error);
-  }
-}
-
-imageData();
