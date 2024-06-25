fa5ca802d1cfa0aefc2f77946cf835f747269f8d
Birm <birm@rbirm.us>
2024-06-25 12:03:09 -0400

    allow participants to review

--- apps/labeling/labelReview.js
+++ apps/labeling/labelReview.js
@@ -168,12 +168,6 @@ async function initialize() {
         // Loading.close();
       }
 
-      // direct user away if they don't have access
-      if (getUserInfo().userType == 'Participant') {
-        alert('Error: Your user account type, \'Participant\', does not have permission to contribute to ROI selection. If you were assigned an ROI selection task and need to change your account type, please contact Brandon.Gallas@fda.hhs.gov.');
-        window.location = '../collection/viewer.html';
-      }
-
       // if (!$D.ROI) redirect($D.pages.table, 'There Is No Label Data, Return Home Page.', 0);
       Loading.open(document.body, 'Loading Data...');
       var checkCoreAndDataIsReady = setInterval(function() {
