be9f7294e9fefd951745c72a41d3097dae61979c
YukaUU <usuidayuu@icloud.com>
2024-07-19 02:27:51 +0900

    fix small bug

--- apps/viewer/init.js
+++ apps/viewer/init.js
@@ -1636,9 +1636,9 @@ function visualizationLayerItems(data) {
 
   // Get initial data
   $D.visualizations.map((d) => {
-    d.geometries.features.map((sd)=>{
-      if (features.viewerStates) {
-        initialZommingData.push(roundToSecondDecimalPlace(features.viewerStates.z));
+    d.geometries.features.map((ditailData)=>{
+      if (ditailData.viewerStates) {
+        initialZommingData.push(roundToSecondDecimalPlace(ditailData.viewerStates.z));
       }
     });
   });
