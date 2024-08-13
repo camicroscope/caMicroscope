055295845d2e81b21ed0bcf54f96896b936c0f3f
YukaUU <usuidayuu@icloud.com>
2024-08-13 15:53:50 +0900

    change allSlides to getVisualizationData

--- components/visualization-dashboard/visualization-init.js
+++ components/visualization-dashboard/visualization-init.js
@@ -1,3 +1,43 @@
 function test() {
-  console.log("result", allSlides);
+  console.log('result', getVisualizationData);
 };
+
+function initialize() {
+  const store = new Store('../data/');
+  store.findSlide()
+      .then(function(data) {
+        if (data.length == 0) {
+          var div = document.querySelector('.container');
+          div.textContent = `No Data Found ... x _ x`;
+          div.classList = `text-center p-4`;
+          return;
+        }
+        for (var i = 0; i < data.length; i++) {
+          const JSONdata={};
+          JSONdata.id=data[i]._id.$oid;
+          JSONdata.name=data[i].name;
+          JSONdata.displayed=true;
+          if (data[i].filter) {
+            JSONdata.filterList = JSON.parse(data[i].filter.replace(/'/g, '"'));
+            if (!JSONdata.filterList.some((filter) => (filters.indexOf(filter) > - 1))) {
+              JSONdata.filterList = ['Others'];
+            }
+          } else {
+            JSONdata.filterList = ['Public'];
+          }
+          store.fetchMark(JSONdata.id).then(function(dataq) {
+            JSONdata.annotations=dataq;
+            store.fetchHeatMap(JSONdata.id).then(function(dataqt) {
+              JSONdata.heatmap=dataqt;
+              // console.log("JSONdata", JSONdata.id);
+              if (slideId == JSONdata.id) {
+                getVisualizationData.push(JSONdata);
+                console.log('aaa');
+              }
+              // addbody(JSONdata);
+            });
+          });
+          //  console.log('a b',getVisualizationData);
+        }
+      });
+}
