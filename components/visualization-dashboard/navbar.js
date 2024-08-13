305b85114cca26f834fb8b8a9f97398150e04e1d
YukaUU <usuidayuu@icloud.com>
2024-08-13 17:24:13 +0900

    fix title

--- components/visualization-dashboard/navbar.js
+++ components/visualization-dashboard/navbar.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const navbar = `
         <div class="navbar">
-            <h1>My Website</h1>
+            <h1>Visualization DashBoard</h1>
             <nav>
                 <ul>
                     <li><span class="material-icons" aria-label="home" role="img">home</span><a href="./Info.html">Home</a></li>
